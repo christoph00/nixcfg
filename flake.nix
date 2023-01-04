dd82525e7e044c8766a94033171e51f169b9fc5d
Christoph <christoph00@users.noreply.github.com>
2023-01-04 12:53:12 +0000

    ok

--- flake.nix
+++ flake.nix
@@ -64,7 +64,7 @@
   };
 
   outputs = inputs@{ flake-parts, ... }:
-      flake-parts.lib.mkFlake { inherit inputs; }
+      flake-parts.lib.mkFlake { inherit inputs; } {
       systems = ["x86_64-linux" "aarch64-linux"];
       imports = [
         ./lib.nix
