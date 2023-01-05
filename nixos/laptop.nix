5aad2f7d50c4d62dbb1d402b7977811252d5f7fa
Christoph Asche <christoph@asche.co>
2023-01-05 17:29:26 +0100

    ok

--- nixos/laptop.nix
+++ nixos/laptop.nix
@@ -43,4 +43,22 @@
       "/var/lib/iwd"
     ];
   };
+
+  systemd.user.timers.notify-on-low-battery = {
+    timerConfig.OnBootSec = "2m";
+    timerConfig.OnUnitInactiveSec = "2m";
+    timerConfig.Unit = "notify-on-low-battery.service";
+    wantedBy = ["timers.target"];
+  };
+
+  systemd.user.services.notify-on-low-battery = {
+    serviceConfig.PassEnvironment = "DISPLAY";
+    script = ''
+      export battery_capacity=$(${pkgs.coreutils}/bin/cat /sys/class/power_supply/BAT0/capacity)
+      export battery_status=$(${pkgs.coreutils}/bin/cat /sys/class/power_supply/BAT0/status)
+      if [[ $battery_capacity -le 10 && $battery_status = "Discharging" ]]; then
+        ${pkgs.libnotify}/bin/notify-send --urgency=critical "$battery_capacity%: See you, space cowboy..."
+      fi
+    '';
+  };
 }
