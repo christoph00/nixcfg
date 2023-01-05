85702719af607e59b449519ba5683b87cfb60bbe
Christoph Asche <christoph@asche.co>
2023-01-05 16:47:09 +0100

    ok

--- home/default.nix
+++ home/default.nix
@@ -8,8 +8,6 @@
     homeModules = {
       common = {
         home.stateVersion = "22.11";
-        nixpkgs.overlays = builtins.attrValues self.overlays;
-        nixpkgs.config.allowUnfree = true;
         imports = [
           inputs.impermanence.nixosModules.home-manager.impermanence
           inputs.nix-colors.homeManagerModule
