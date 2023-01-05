0e5fd2da0ee5c105667449dc07a202f27565b433
Christoph Asche <christoph@asche.co>
2023-01-05 22:31:40 +0100

    ok

--- home/desktop/default.nix
+++ home/desktop/default.nix
@@ -66,6 +66,21 @@ in {
       name = "Papirus-Dark";
     };
   };
+
+  services.gammastep = {
+    enable = true;
+    provider = "manual";
+    duskTime = "18:35-20:15";
+    dawnTime = "6:00-7:45";
+    temperature = {
+      day = 5500;
+      night = 3700;
+    };
+    settings = {
+      general.adjustment-method = "wayland";
+    };
+  };
+
   home.sessionVariables = {
     MOZ_ENABLE_WAYLAND = "true";
     QT_QPA_PLATFORM = "wayland";
