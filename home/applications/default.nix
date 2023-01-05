c7a56f81f743947e63cd20b948d334ed0573868d
Christoph Asche <christoph@asche.co>
2023-01-05 16:53:33 +0100

    ok

--- home/applications/default.nix
+++ home/applications/default.nix
@@ -4,5 +4,5 @@
   lib,
   ...
 }: {
-  imports = [./firefox.nix ./wezterm.nix ./helix.nix ./vscode.nix ./kitty.nix];
+  imports = [./firefox.nix ./wezterm.nix ./helix.nix ./vscode.nix ./kitty.nix ./office.nix];
 }
