4532123fd4c2c34842f4e50988e93262d28132ed
Christoph Asche <christoph@asche.co>
2023-01-06 21:45:06 +0100

    ok

--- home/applications/firefox.nix
+++ home/applications/firefox.nix
@@ -6,6 +6,7 @@
   ...
 }: let
   addons = inputs.firefox-addons.packages.${pkgs.system};
+  inherit (config.colorscheme) colors;
 in {
   programs.browserpass.enable = true;
   programs.firefox = {
@@ -33,7 +34,6 @@ in {
         "browser.shell.checkDefaultBrowser" = false;
         "browser.shell.defaultBrowserCheckCount" = 1;
         "browser.disableResetPrompt" = true;
-        "browser.uiCustomization.state" = ''{"placements":{"widget-overflow-fixed-list":["ublock0_raymondhill_net-browser-action"],"nav-bar":["back-button","forward-button","stop-reload-button","home-button","urlbar-container","downloads-button","library-button"],"toolbar-menubar":["menubar-items"],"TabsToolbar":["tabbrowser-tabs","new-tab-button","alltabs-button"],"PersonalToolbar":["import-button","personal-bookmarks"]},"seen":["save-to-pocket-button","developer-button","ublock0_raymondhill_net-browser-action"],"dirtyAreaCache":["nav-bar","PersonalToolbar","toolbar-menubar","TabsToolbar","widget-overflow-fixed-list"],"currentVersion":17,"newElementCount":3}'';
         "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
         "svg.context-properties.content.enabled" = true;
         "browser.ctrlTab.sortByRecentlyUsed" = true;
@@ -50,24 +50,20 @@ in {
         "widget.dmabuf.force-enabled" = true;
         "widget.use-xdg-desktop-portal" = true;
 
-        "gnomeTheme.hideSingleTab" = true;
         "gnomeTheme.systemIcons" = true;
+        "gnomeTheme.hideSingleTab" = true;
       };
       userChrome = ''
         @import "${pkgs.firefox-gnome-theme}/share/firefox-gnome-theme/userChrome.css";
 
         @media (prefers-color-scheme: dark) {
          :root {
-           /* Accent */
-          --gnome-accent: #${config.colorscheme.colors.base08};
 
-          /* Buttons */
-          --gnome-button-suggested-action-background: linear-gradient(to top, rgb(36, 235, 195) 2px, #16A085);
-          --gnome-button-suggested-action-border-color: #16A085;
-          --gnome-button-suggested-action-border-accent-color: #004b3d;
-          --gnome-button-suggested-action-hover-background: linear-gradient(to top, rgb(36, 235, 195), #16A085 1px);
-          --gnome-button-suggested-action-active-background: rgb(16, 129, 107);
-          --gnome-button-suggested-action-active-border-color: rgb(13, 109, 90);
+          --gnome-browser-before-load-background: #${colors.base00};
+          --gnome-headerbar-background: #${colors.base00};
+
+          --gnome-accent: #${colors.base08};
+
          }
         }
       '';
