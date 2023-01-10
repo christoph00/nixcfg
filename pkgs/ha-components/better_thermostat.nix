d7ba946164e8f1118fe237d18be8ef681d3ea85d
Christoph Asche <christoph@asche.co>
2023-01-10 19:04:12 +0100

    update better_thermostat

--- pkgs/ha-components/better_thermostat.nix
+++ pkgs/ha-components/better_thermostat.nix
@@ -4,13 +4,13 @@
 }:
 stdenvNoCC.mkDerivation rec {
   pname = "ha-component-better-thermostat";
-  version = "1.0.0-beta55";
+  version = "1.0.0-beta56";
 
   src = fetchFromGitHub {
     owner = "KartoffelToby";
     repo = "better_thermostat";
     rev = version;
-    sha256 = "10fv3biv1b2x7cq3jn79j4k62c3v7j35521c11k4bxxf4nxpzjhy";
+    sha256 = "02922j7ymy0fczx1wdqwr158c3ddgmxf5jfyfyam4i1df0fvfq80";
   };
 
   installPhase = ''
