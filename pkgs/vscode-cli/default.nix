12f6bc8d07b34f1e5c3a019d86f611de4bec8ff0
Christoph Asche <christoph@asche.co>
2023-01-10 20:22:25 +0100

    ok

--- pkgs/vscode-cli/default.nix
+++ pkgs/vscode-cli/default.nix
@@ -1,17 +1,14 @@
 {
   pkgs,
-  stdenvNoCC,
+  stdenv,
   autoPatchelfHook,
   fetchurl,
-  buildFHSUserEnv,
-  stdenv,
   ...
 }: let
   inherit (stdenv.hostPlatform) system;
 
   throwSystem = throw "Unsupported system: ${system}";
 
-  version = "1.74.2";
   plat =
     {
       x86_64-linux = "cli-alpine-x64";
@@ -29,38 +26,19 @@
     }
     .${system}
     or throwSystem;
-
-  vscode-cli-unwrapped = stdenvNoCC.mkDerivation {
-    pname = "vscode-cli-unwrapped";
-    inherit version;
+in
+  stdenv.mkDerivation rec {
+    pname = "vscode-cli";
+    version = "1.74.2";
     src = fetchurl {
-      name = "VSCode-CLI_${version}_${plat}.tar..gz";
+      name = "VSCode-CLI_${version}_${plat}.tar.gz";
+
       url = "https://update.code.visualstudio.com/${version}/${plat}/stable";
 
       inherit hash;
     };
-    #nativeBuildInputs = [autoPatchelfHook];
-    #buildInputs = [];
+    nativeBuildInputs = [autoPatchelfHook];
+    buildInputs = [];
     sourceRoot = ".";
     installPhase = ''install -m755 -D code $out/bin/code-cli '';
-  };
-
-  env = buildFHSUserEnv {
-    name = "vscode-cli-${version}";
-    targetPkgs = _: [vscode-cli-unwrapped];
-    runScript = "vscode-cli";
-  };
-in
-  stdenvNoCC.mkDerivation {
-    pname = "vscode-cli";
-    inherit version;
-
-    dontUnpack = true;
-    dontConfigure = true;
-    dontBuild = true;
-
-    installPhase = ''
-      mkdir -p $out/bin
-      ln -s ${env}/bin/* $out/bin/vscode-cli
-    '';
   }
