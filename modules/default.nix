e207f75b6c8d34d5ffa9e2990c70c15511f46789
Christoph Asche <christoph@asche.co>
2023-01-13 20:26:34 +0100

    ok

--- modules/default.nix
+++ modules/default.nix
@@ -2,5 +2,6 @@
   flake = {
     homeManagerModules.default.imports = [./home-manager];
     nixosModules.sftpgo.imports = [./nixos/sftpgo.nix];
+    nixosModules.gotosocial.imports = [./nixos/gotosocial.nix];
   };
 }
