37f40219a733889a01cb365bf0b0aee9df61e462
Christoph Asche <christoph@asche.co>
2023-01-13 21:31:57 +0100

    ok

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -e
+
+USAGE=<<EOF
+apply.sh {dry-build|build|activate|dry-activate|switch} TARGET_HOST [ ... ]
+
+This expects to be run from under the flake repository.
+Extra args are passed to nixos-rebuild directly.
+EOF
+
+local_hostname="$(hostname -s)"
+command="${1?}"
+target="${2?}"
+shift 2
+extra_args=$*
+
+if [ $target == $local_hostname ]; then
+	set -x
+	sudo nixos-rebuild --flake .# "${command?}" ${extra_args}
+else
+	set -x
+	NIX_SSHOPTS=-A nixos-rebuild "${command?}" --flake ".#${target?}" --target-host "${target?}.lan.net.r505.de" --use-remote-sudo --use-substitutes ${extra_args}
+fi
+
