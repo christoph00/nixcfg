16553c38a531680e9afae798ea20276e5918a611
Christoph Asche <christoph@asche.co>
2023-01-13 23:43:49 +0100

    ok

--- secrets.nix
+++ secrets.nix
@@ -7,12 +7,14 @@ let
   air13 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJaQ1qn7oju1z6X2mumCSg+bsTCNlgzE5KahvO2BxKtg";
   futro = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHkYJjXoEofN3Nb/b9Dxsc0+J2S5fUU7fZOs6hqZCvGT";
   tower = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH5CLg7gUc4TAu49l7wlRGS4v9JXY3CR0IJUQIlrQ4bl";
+  oca = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGoZYVHRKCEs5lRYEIr1OgkydoPiGpVaUGStAIYakXgI";
+  oc1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGeJMB9F6JIthu6pltQqAH4FexyNeP+haAkEy5juJrdC";
 in {
-  "secrets/cachix".publicKeys = [christoph_air13 air13 futro tower];
-  "secrets/tailscale-preauthkey".publicKeys = [christoph_air13 air13 futro tower];
-  "secrets/cf-acme".publicKeys = [christoph_air13 futro];
+  "secrets/cachix".publicKeys = [christoph_air13 air13 futro tower oca oc1];
+  "secrets/tailscale-preauthkey".publicKeys = [christoph_air13 air13 futro tower oca oc1];
+  "secrets/cf-acme".publicKeys = [christoph_air13 futro oca oc1];
   "secrets/futro-cf".publicKeys = [christoph_air13 futro];
   "secrets/ha-serviceaccount".publicKeys = [christoph_air13 futro];
   "secrets/ha-secrets.yaml".publicKeys = [christoph_air13 futro];
-  "secrets/christoph-password.age".publicKeys = [christoph_air13 futro air13 tower];
+  "secrets/christoph-password.age".publicKeys = [christoph_air13 futro air13 tower oca oc1];
 }
