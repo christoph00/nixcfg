0f6974aca94924376819aebb50630cbf23aac070
Christoph Asche <christoph@asche.co>
2023-01-13 21:31:42 +0100

    ok

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -11,6 +11,7 @@
   ha-lovelace-fold-entity-row = pkgs.callPackage ./ha-lovelace/fold-entity-row.nix {};
   ha-lovelace-mini-graph-card = pkgs.callPackage ./ha-lovelace/mini-graph-card.nix {};
   ha-lovelace-card-mod = pkgs.callPackage ./ha-lovelace/card-mod.nix {};
+  ha-lovelace-better-thermostat-ui-card = pkgs.callPackage ./ha-lovelace/better-thermostat-ui-card.nix {};
   ha-components-better-thermostat = pkgs.callPackage ./ha-components/better_thermostat.nix {};
   ha-components-ble-monitor = pkgs.callPackage ./ha-components/ble_monitor.nix {};
 }
