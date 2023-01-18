54d7255dbff83c982c9d0cb44d7c90b61827e835
Christoph Asche <christoph@asche.co>
2023-01-18 19:43:39 +0100

    oc1

--- hosts/oc1/default.nix
+++ hosts/oc1/default.nix
@@ -28,6 +28,6 @@
 
   # ----------  Secrets  -----------------------------------------
   #age.secrets.cloudflared.file = ../../secrets/oca-cf;
-  #age.secrets.tailscale-preauthkey.file = ../../secrets/tailscale-preauthkey;
+  age.secrets.tailscale-preauthkey.file = ../../secrets/tailscale-preauthkey;
   #age.secrets.cf-acme.file = ../../secrets/cf-acme;
 }
