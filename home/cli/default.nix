ffb8edae230184683043d9461bce9a7b3b2f0061
Christoph Asche <christoph@asche.co>
2023-01-18 18:54:32 +0100

    zellij

--- home/cli/default.nix
+++ home/cli/default.nix
@@ -2,8 +2,10 @@
   pkgs,
   inputs,
   system,
+  config,
   ...
-}: let
+}:
+with config.colorscheme; let
   localeGerman = "de_DE.UTF-8";
   localeEnglish = "en_US.UTF-8";
 in {
@@ -91,7 +93,37 @@ in {
     home-manager.enable = true;
     lazygit.enable = true;
     nnn.enable = true;
-    zellij.enable = true;
+  };
+
+  xdg.configFile."zellij/themes/${slug}.kdl".text = ''
+    themes {
+      ${slug} {
+        fg "#${colors.base05}"
+        bg "#${colors.base00}"
+        black "#${colors.base03}"
+        red "#${colors.base08}"
+        green "#${colors.base0B}"
+        yellow "#${colors.base0A}"
+        blue "#${colors.base0D}"
+        magenta "#${colors.base0E}"
+        cyan "#${colors.base0C}"
+        white "#${colors.base05}"
+        orange "#${colors.base0F}"
+      }
+    }
+  '';
+
+  programs.zellij = {
+    enable = true;
+    settings = {
+      default_mode = "normal";
+      ui.pane_frames.rounded_corners = true;
+      default_layout = "compact";
+      simplified_ui = true;
+      pane_frames = true;
+      scrollback_editor = "hx";
+      theme = "${slug}";
+    };
   };
 
   programs.bash = {
