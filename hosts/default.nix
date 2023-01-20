7cc612e60423364d3447f6f99f47d10ef549c9d7
Christoph Asche <christoph@asche.co>
2023-01-20 21:47:48 +0100

    ok

--- hosts/default.nix
+++ hosts/default.nix
@@ -19,7 +19,7 @@
         modules = [
           self.nixosModules.desktop
           self.nixosModules.home-desktop-christoph
-          self.nixosModules.gamestream
+          #self.nixosModules.gamestream
           #self.nixosModules.code-server
           ./tower
         ];
