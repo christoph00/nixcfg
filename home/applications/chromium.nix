77ba92bcc00d8d07de471ec640d4ce10de893f59
Christoph Asche <christoph@asche.co>
2023-01-29 21:23:49 +0100

    ok

--- home/applications/chromium.nix
+++ home/applications/chromium.nix
@@ -2,10 +2,42 @@
   config,
   pkgs,
   ...
-}: {
+}: let
+  vivaldi-css = with config.colorscheme.colors; ''
+    :root {
+      --base00: #${base00}; # ----
+      --base01: #${base01}; # ---
+      --base02: #${base02}; # --
+      --base03: #${base03}; # -
+      --base04: #${base04}; # +
+      --base05: #${base05}; # ++
+      --base06: #${base06}; # +++
+      --base07: #${base07}; # ++++
+      --base08: #${base08}; # red
+      --base09: #${base09}; # orange
+      --base0A: #${base0A}; # yellow
+      --base0B: #${base0B}; # green
+      --base0C: #${base0C}; # aqua
+      --base0D: #${base0D}; # blue
+      --base0E: #${base0E}; # purple
+      --base0F: #${base0F}; # brown
+    }
+
+    * {
+      font-family: ${config.fontProfiles.regular.family};
+    }
+
+  '';
+in {
   programs.chromium = {
     enable = true;
-    package = pkgs.chromium;
+    #package = pkgs.vivaldi;
+    package = pkgs.vivaldi.override {
+      proprietaryCodecs = true;
+      enableWidevine = true;
+      vivaldi-ffmpeg-codecs = pkgs.vivaldi-ffmpeg-codecs;
+      vivaldi-widevine = pkgs.vivaldi-widevine;
+    };
     extensions = [
       {id = "cjpalhdlnbpafiamejdnhcphjbkeiagm";} # Ublock Origin
       {id = "jhnleheckmknfcgijgkadoemagpecfol";} # Auto-Tab-Discard
@@ -25,9 +57,10 @@
     ];
   };
 
-  nixpkgs.config.chromium.commandLineArgs = "--enable-features=UseOzonePlatform --ozone-platform=wayland";
+  home.packages = [pkgs.vivaldi-widevine pkgs.vivaldi-ffmpeg-codecs];
 
   home.persistence = {
-    "/nix/persist/home/christoph".directories = [".config/chromium/Default" ".config/BraveSoftware/Brave-Browser/Default"];
+    "/nix/persist/home/christoph".directories = [".config/chromium/Default" ".config/BraveSoftware/Brave-Browser/Default" ".config/vivaldi/Default"];
   };
+  home.file.".config/vivaldi/UserCSS/theme.css".text = vivaldi-css;
 }
