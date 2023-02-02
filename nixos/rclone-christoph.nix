175a576ddf4c5a6aaa6f9de5b67bce3bd2ade1aa
Christoph Asche <christoph@asche.co>
2023-02-02 20:30:36 +0100

    ok

--- nixos/rclone-christoph.nix
+++ nixos/rclone-christoph.nix
@@ -4,19 +4,19 @@
   lib,
   ...
 }: {
-  systemd.services.rclone-christoph = {
-    serviceConfig.Type = "oneshot";
-    serviceConfig.User = "christoph";
-    script = ''${pkgs.rclone}/bin/rclone --config ${config.age.secrets.rclone-conf.path} bisync snas:Dokumente ~/Dokumente'';
-  };
-  systemd.timers.rclone-christoph = {
-    wantedBy = ["timers.target"];
-    partOf = ["rclone-christoph.service"];
-    timerConfig = {
-      OnCalendar = "*:0/05";
-      Unit = "rclone-christoph.service";
-    };
-  };
+  # systemd.services.rclone-christoph = {
+  #   serviceConfig.Type = "oneshot";
+  #   serviceConfig.User = "christoph";
+  #   script = ''${pkgs.rclone}/bin/rclone --config ${config.age.secrets.rclone-conf.path} bisync snas:Dokumente ~/Dokumente'';
+  # };
+  # systemd.timers.rclone-christoph = {
+  #   wantedBy = ["timers.target"];
+  #   partOf = ["rclone-christoph.service"];
+  #   timerConfig = {
+  #     OnCalendar = "*:0/05";
+  #     Unit = "rclone-christoph.service";
+  #   };
+  # };
 
   systemd.services.mount-christoph-nas = {
     description = "Mount christoph/NAS";
