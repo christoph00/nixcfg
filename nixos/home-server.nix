cbe4e0b1698eda026f98e7b8eb54e9b6c7854d00
Christoph Asche <christoph@asche.co>
2023-02-04 07:48:01 +0100

    ""

--- nixos/home-server.nix
+++ nixos/home-server.nix
@@ -4,7 +4,7 @@
   lib,
   ...
 }: {
-  networking.firewall.allowedTCPPorts = [1883 53 8096 8030 80 443 2022];
+  networking.firewall.allowedTCPPorts = [1883 53 8096 8030 80 443 2022 9100];
   networking.firewall.allowedUDPPorts = [53];
 
   users.users.sftpgo.extraGroups = ["media"];
@@ -58,6 +58,11 @@
       #   directory = "/var/lib/sabnzbd";
       #   inherit (config.services.sabnzbd) user group;
       # }
+      {
+        directory = "/var/lib/prometheus2";
+        user = "prometheus";
+        group = "prometheus";
+      }
     ];
   };
 
