05ac5fa01d0393c94fecb46400e6bc0217d0a216
Christoph Asche <christoph@asche.co>
2023-05-10 22:58:52 +0200

    /

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -8,7 +8,7 @@ final: prev: {
         androidtvremote2 = python-final.callPackage ./python/androidtvremote2.nix {};
         faster-whisper = python-final.callPackage ./python/faster-whisper.nix {};
 
-        python-androidtv = prev.python-androidtv.overrideAttrs (o: {
+        androidtv = prev.androidtv.overrideAttrs (o: {
           patches =
             (o.patches or [])
             ++ [
