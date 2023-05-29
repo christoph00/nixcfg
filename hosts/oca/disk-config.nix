438083007a165315c804021079a356dec25e5374
Christoph Asche <christoph@asche.co>
2023-05-29 22:32:12 +0200

    ok

---
+++ hosts/oca/disk-config.nix
@@ -0,0 +1,54 @@
+{disks ? ["/dev/vda"], ...}: {
+    disk = {
+      main = {
+        type = "disk";
+        device = builtins.elemAt disks 0;
+        content = {
+          type = "table";
+          format = "gpt";
+          partitions = [
+             {
+              name = "ESP";
+              start = "1MiB";
+              end = "128MiB";
+              fs-type = "fat32";
+              bootable = true;
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            }
+            {
+              name = "root";
+              start = "128MiB";
+              end = "100%";
+              content = {
+                type = "btrfs";
+                extraArgs = ["-f"]; # Override existing partition
+                subvolumes = {
+                  # Subvolume name is different from mountpoint
+                  "/@boot" = {
+                    mountpoint = "/boot";
+                  };
+                  # Mountpoints inferred from subvolume name
+                  "/@home" = {
+                    mountOptions = ["compress=zstd"];
+                    mountpoint = "/home";
+                  };
+                  "/@nix" = {
+                    mountOptions = ["compress=zstd" "noatime"];
+                    mountpoint = "/nix";
+                  };
+                  "/@persist" = {
+                    mountOptions = ["compress=zstd" "noatime"];
+                    mountpoint = "/nix/persist";
+                  };
+                };
+              };
+            }
+          ];
+        };
+      };
+    };
+}
