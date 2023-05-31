534a23c51e2946f251620477210639e381b6fe3d
Christoph Asche <christoph@asche.co>
2023-05-31 20:41:28 +0200

    ok

--- nixos/wg-pia.nix
+++ nixos/wg-pia.nix
@@ -4,14 +4,12 @@
   lib,
   ...
 }: {
-   services.pia-vpn = {
-      enable = true;
-      certificateFile = config.age.secrets.pia-crt.path;
-      environmentFile = config.age.secrets.pia-env.path;
-    };
+  services.pia-vpn = {
+    enable = true;
+    certificateFile = config.age.secrets.pia-crt.path;
+    environmentFile = config.age.secrets.pia-env.path;
+  };
 
-      age.secrets.pia-env.file = ../secrets/pia.env;
-      age.secrets.pia-crt.file = ../secrets/pia.crt;
-
-
+  age.secrets.pia-env.file = ../secrets/pia.env;
+  age.secrets.pia-crt.file = ../secrets/pia.crt;
 }
