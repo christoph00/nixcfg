b038a0400769fcc615775c7eb3f376b0f50aa678
Christoph <christoph00@users.noreply.github.com>
2023-05-31 11:51:05 +0200

    pia

--- modules/default.nix
+++ modules/default.nix
@@ -3,5 +3,6 @@
     homeManagerModules.default.imports = [./home-manager];
     nixosModules.autologin-graphical-session.imports = [./nixos/autologin-graphical-session.nix];
     nixosModules.tailscale-tls.imports = [./nixos/tailscale-tls.nix];
+    nixosModules.pia-vpn.imports = [./nixos/pia-vpn.nix];
   };
 }
--- nixos/wg-pia.nix
+++ nixos/wg-pia.nix
@@ -4,43 +4,9 @@
   lib,
   ...
 }: {
-  networking.firewall = {
-    allowedUDPPorts = [51820]; # Clients and peers can use the same port, see listenport
-  };
-  # Enable WireGuard
-  networking.wireguard.interfaces = {
-    # "wg0" is the network interface name. You can name the interface arbitrarily.
-    wg0 = {
-      # Determines the IP address and subnet of the client's end of the tunnel interface.
-      ips = ["10.15.200.49/32"];
-      listenPort = 51820; # to match firewall allowedUDPPorts (without this wg uses random port numbers)
-
-      # Path to the private key file.
-      #
-      # Note: The private key can also be included inline via the privateKey option,
-      # but this makes the private key world-readable; thus, using privateKeyFile is
-      # recommended.
-      privateKeyFile = config.age.secrets.wg-pia-key.path;
-
-      peers = [
-        # For a client configuration, one peer entry for the server will suffice.
-
-        {
-          # Public key of the server (not a file path).
-          publicKey = "HuwmjIO54wtTa5ZW0q84bCC1mPOvnggNYGTE1oDcLRY=";
-
-          # Forward all the traffic via VPN.
-          #allowedIPs = ["0.0.0.0/0"];
-          # Or forward only particular subnets
-          allowedIPs = [ "10.0.0.0/8" ];
-
-          # Set this to the server IP and port.
-          endpoint = "212.102.57.80:1337"; # ToDo: route to endpoint not automatically configured https://wiki.archlinux.org/index.php/WireGuard#Loop_routing https://discourse.nixos.org/t/solved-minimal-firewall-setup-for-wireguard-client/7577
-
-          # Send keepalives every 25 seconds. Important to keep NAT tables alive.
-          persistentKeepalive = 25;
-        }
-      ];
+   services.pia-vpn = {
+      enable = true;
+      certificateFile = "ca.rsa.4096.crt";
+      environmentFile = "pia.env";
     };
-  };
 }
