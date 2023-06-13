96df93fb166e4bc57d7bd6cff377cc64f414d3df
Christoph Asche <christoph@asche.co>
2023-06-13 17:42:36 +0200

    ok

---
+++ pkgs/home-gallery/default1.nix
@@ -0,0 +1,24 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+}:
+stdenv.mkDerivation rec {
+  pname = "home-gallery";
+  version = "1.10.0";
+
+  src = fetchFromGitHub {
+    owner = "xemle";
+    repo = "home-gallery";
+    rev = "v${version}";
+    hash = "sha256-RfBb0i7yDs+Wj7OTVW0gemhaI9LQCvcxDqAgj8Wibn8=";
+  };
+
+  meta = with lib; {
+    description = "Self-hosted open-source web gallery to view your photos and videos featuring mobile-friendly, tagging and AI powered image discovery";
+    homepage = "https://github.com/xemle/home-gallery";
+    changelog = "https://github.com/xemle/home-gallery/blob/${src.rev}/CHANGELOG.md";
+    license = licenses.mit;
+    maintainers = with maintainers; [];
+  };
+}
