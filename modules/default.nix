ddfb8cc8aa96a4bf7635e164ba812bcb9df731e6
Christoph Asche <christoph@asche.co>
2023-07-01 23:09:58 +0200

    ok

--- modules/default.nix
+++ modules/default.nix
@@ -4,5 +4,7 @@
     nixosModules.autologin-graphical-session.imports = [./nixos/autologin-graphical-session.nix];
     nixosModules.tailscale-tls.imports = [./nixos/tailscale-tls.nix];
     nixosModules.pia-vpn.imports = [./nixos/pia-vpn.nix];
+    nixosModules.feed2imap.imports = [./nixos/feed2imap.nix];
+
   };
 }
