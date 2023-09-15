03827a926e5367ccb9cf85c80e0a6bb146181dd8
Christoph Asche <christoph@asche.co>
2023-09-15 22:13:33 +0200

    snowfall

--- flake.nix
+++ flake.nix
@@ -14,9 +14,14 @@
     unfree.url = "github:numtide/nixpkgs-unfree/nixos-unstable";
     unfree.inputs.nixpkgs.follows = "nixpkgs";
 
-    flake-parts = {
-      url = "github:hercules-ci/flake-parts";
-      inputs.nixpkgs-lib.follows = "nixpkgs";
+    snowfall-lib = {
+      url = "github:snowfallorg/lib";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    snowfall-frost = {
+      url = "github:snowfallorg/frost";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
 
     home-manager = {
@@ -39,11 +44,6 @@
 
     nixos-hardware.url = "github:nixos/nixos-hardware";
 
-    devshell = {
-      url = "github:numtide/devshell";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
     disko = {
       url = github:nix-community/disko;
       inputs.nixpkgs.follows = "nixpkgs";
@@ -57,36 +57,6 @@
     nix-colors = {
       url = "github:misterio77/nix-colors";
       inputs.nixpkgs-lib.follows = "nixpkgs";
-    };
-
-    hyprland = {
-      url = "github:hyprwm/hyprland";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    hyprpicker = {
-      url = "github:hyprwm/hyprpicker";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    xdg-portal-hyprland = {
-      url = "github:hyprwm/xdg-desktop-portal-hyprland";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    hyprland-contrib = {
-      url = "github:hyprwm/contrib";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    hy3 = {
-      url = "github:outfoxxed/hy3";
-      inputs.hyprland.follows = "hyprland";
-    };
-
-    eww = {
-      url = "github:elkowar/eww";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.rust-overlay.follows = "rust-overlay";
     };
 
     anyrun = {
@@ -104,43 +74,14 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    tsnsrv = {
-      url = "github:boinkor-net/tsnsrv";
-      inputs.flake-parts.follows = "flake-parts";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
     ags = {
       url = "github:Aylur/ags";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-
-  outputs = {
-    self,
-    nixpkgs,
-    flake-parts,
-    ...
-  } @ inputs:
-    flake-parts.lib.mkFlake {inherit inputs;} ({withSystem, ...}: {
-      systems = ["x86_64-linux" "aarch64-linux"];
-      imports = [
-        {config._module.args._inputs = inputs // {inherit (inputs) self;};}
-        ./pkgs
-      ];
-      flake = let
-        lib = import ./lib {inherit nixpkgs lib inputs;};
-      in {
-        nixosConfigurations = import ./hosts {inherit nixpkgs self lib withSystem;};
-      };
-      perSystem = {
-        config,
-        inputs',
-        pkgs,
-        system,
-        ...
-      }: {
-        formatter = pkgs.alejandra;
-      };
-    });
+  outputs = inputs:
+    inputs.snowfall-lib.mkFlake {
+      inherit inputs;
+      src = ./.;
+    };
 }
