1ab42fa69b74d5934d024478731f443bb55deeee
Christoph <christoph00@users.noreply.github.com>
2023-09-15 14:21:05 +0200

    ok

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
   ];
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     unfree.url = "github:numtide/nixpkgs-unfree/nixos-unstable";
     unfree.inputs.nixpkgs.follows = "nixpkgs";
