f194656b758a6b229073456fc2406573fc9cae66
Christoph <christoph00@users.noreply.github.com>
2023-09-19 09:07:22 +0200

    ok

---
+++ modules/nixos/system/type/default.nix
@@ -0,0 +1,24 @@
+{ options, config, lib, pkgs, ... }:
+with lib;
+with lib.chr;
+let cfg = config.chr.type;
+in
+{
+ options.chr.type = mkOption {
+      type = types.enum ["laptop" "desktop" "server" "vm"];
+    };
+
+ config = mkMerge [
+    (mkIf (cfg == "server"){
+
+
+    } ) 
+
+    (mkIf (cfg == "desktop"){
+
+
+    } ) 
+
+ ]
+
+}
