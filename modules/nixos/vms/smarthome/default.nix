91a686cf280dc28df165d753e0641d914c7481ec
Christoph <christoph00@users.noreply.github.com>
2024-02-13 09:28:17 +0000

    ok

---
+++ modules/nixos/vms/smarthome/default.nix
@@ -0,0 +1,23 @@
+{
+  inputs,
+  options,
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+with lib;
+with lib.chr; let
+  cfg = config.chr.vms.smarthome;
+in {
+  options.chr.vms.smarthome = with types; {
+    enable = mkBoolOpt' false;
+  };
+
+  config = mkIf cfg.enable {
+    microvm.vms.vm-smarthome = {
+      flake = inputs.self;
+      updateFlake = "github:christoph00/nixcfg";
+    };
+  };
+}
---
+++ systems/x86_64-linux/vm-smarthome/default.nix
@@ -0,0 +1,38 @@
+{
+  pkgs,
+  config,
+  ...
+}: {
+  chr = {
+    type = "microvm";
+  };
+
+  microvm = {
+    hypervisor = "cloud-hypervisor";
+    mem = 1024;
+    vcpu = 2;
+
+    shares =
+      [
+        {
+          source = "/nix/store";
+          mountPoint = "/nix/.ro-store";
+          tag = "store";
+          proto = "virtiofs";
+          socket = "store.socket";
+        }
+      ]
+      ++ map
+      (dir: {
+        source = "/var/lib/microvms/${config.networking.hostName}/${dir}";
+        mountPoint = "/${dir}";
+        tag = dir;
+        proto = "virtiofs";
+        socket = "${dir}.socket";
+      }) ["etc" "var" "home"];
+  };
+
+  networking = {
+    hostName = "vm-smarthome";
+  };
+}
