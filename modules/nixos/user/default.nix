453b6b953aa4903c6caf0e8241ea455b3c95fb62
Christoph <christoph00@users.noreply.github.com>
2024-04-08 08:14:07 +0200

    Update default.nix

--- modules/nixos/user/default.nix
+++ modules/nixos/user/default.nix
@@ -25,6 +25,8 @@ in {
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICRlMoMsGWPbUR9nC0XavzLmcolpF8hRbvQYALJQNMg8 christoph@tower"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC5KqxXvpZ+R7/GYx99+W0rPHatXKf6/pG6rZ8z81/f6 christoph@x13"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPLfdKX4lkZqV5KlVfu2XyAUrg3IhI7NN3/gCD1f/VRG christoph@oca"
+"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGBdHb/vbNAa+PzunW66VbDlfg2RLxzrok7zweie
+H25D pkey"
     ] "Authorized Keys.";
 
     extraOptions = mkOpt attrs {} "Extra options.";
