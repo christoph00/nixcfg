bb5c7083c4a7c9bfe9ef20b2035d64f476773a60
christoph <christoph@asche.co>
2024-05-02 10:37:59 +0200

    ok

---
+++ devenv.nix
@@ -0,0 +1,25 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  # https://devenv.sh/basics/
+
+  # https://devenv.sh/packages/
+  packages = [ pkgs.git pkgs.alejandra pkgs.nixfmt-rfc-style ];
+
+  # https://devenv.sh/scripts/
+
+
+  # https://devenv.sh/services/
+  # services.postgres.enable = true;
+
+  # https://devenv.sh/languages/
+  # languages.nix.enable = true;
+
+  # https://devenv.sh/pre-commit-hooks/
+  # pre-commit.hooks.shellcheck.enable = true;
+
+  # https://devenv.sh/processes/
+  # processes.ping.exec = "ping example.com";
+
+  # See full reference at https://devenv.sh/reference/options/
+}
