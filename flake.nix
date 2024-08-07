a09eb3d66f050f9ada8bb56f11654a5ea9b9ff5f
christoph00 <christoph@asche.co>
2024-08-07 10:54:00 +0200

    ok

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "nixcfg";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager.url = "github:nix-community/home-manager/release-24.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    snowfall-lib.url = "github:snowfallorg/lib?ref=v3.0.3";
+    snowfall-lib.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs =
+    inputs:
+    let
+      lib = inputs.snowfall-lib.mkLib {
+        inherit inputs;
+        src = ./.;
+      };
+    in
+    lib.mkFlake {
+      channels-config = {
+        allowUnfree = true;
+        permittedInsecurePackages = [
+          "electron-25.9.0"
+          "electron-27.3.11"
+        ];
+      };
+
+      overlays = with inputs; [ flake.overlays.default ];
+
+      systems.modules.nixos = with inputs; [ home-manager.nixosModules.home-manager ];
+
+      deploy = lib.mkDeploy { inherit (inputs) self; };
+
+      checks = builtins.mapAttrs (
+        system: deploy-lib: deploy-lib.deployChecks inputs.self.deploy
+      ) inputs.deploy-rs.lib;
+
+      outputs-builder = channels: { formatter = channels.nixpkgs.nixfmt-rfc-style; };
+    }
+    // {
+      self = inputs.self;
+    };
+
+}
