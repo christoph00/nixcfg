acb27afdbffc6848c43b26429010d57c8f39d0fd
christoph00 <christoph@asche.co>
2024-08-07 11:00:49 +0200

    ok

---
+++ modules/nixos/user/default.nix
@@ -0,0 +1,100 @@
+{
+  options,
+  config,
+  pkgs,
+  lib,
+  namespace,
+  ...
+}:
+let
+  inherit (lib) mDoc types listOf;
+  inherit (lib.${namespace}) mkOpt mkBoolOpt;
+
+  cfg = config.${namespace}.user;
+  defaultIconFileName = "profile.png";
+
+in
+{
+  options.${namespace}.user = with types; {
+    name = mkOpt str "christoph" "The name to use for the user account.";
+    fullName = mkOpt str "Christoph" "The full name of the user.";
+    email = mkOpt str "christoph@asche.co" "The email of the user.";
+    initialPassword =
+      mkOpt str "hallo11"
+        "The initial password to use when the user is first created.";
+    prompt-init = mkBoolOpt true "Whether or not to show an initial message when opening a new shell.";
+    extraGroups = mkOpt (listOf str) [ ] "Groups for the user to be assigned.";
+    extraOptions = mkOpt attrs { } (mdDoc "Extra options passed to `users.users.<name>`.");
+  };
+
+  config = {
+
+    programs.zsh = {
+      enable = true;
+      autosuggestions.enable = true;
+      histFile = "$XDG_CACHE_HOME/zsh.history";
+    };
+
+    ${namespace}.home = {
+      file = {
+        "Desktop/.keep".text = "";
+        "Documents/.keep".text = "";
+        "Downloads/.keep".text = "";
+        "Music/.keep".text = "";
+        "Pictures/.keep".text = "";
+        "Videos/.keep".text = "";
+        "Code/.keep".text = "";
+      };
+
+      extraOptions = {
+         programs = {
+          starship = {
+            enable = true;
+            settings = {
+              character = {
+                success_symbol = "[➜](bold green)";
+                error_symbol = "[✗](bold red) ";
+                vicmd_symbol = "[](bold blue) ";
+              };
+            };
+          };
+
+          zsh = {
+            enable = true;
+            enableCompletion = true;
+            syntaxHighlighting.enable = true;
+
+            autosuggestion.enable = true;
+
+            initExtra =
+              ''
+                # Fix an issue with tmux.
+                export KEYTIMEOUT=1
+
+                # Use vim bindings.
+                set -o vi
+
+                # Improved vim bindings.
+                source ${pkgs.zsh-vi-mode}/share/zsh-vi-mode/zsh-vi-mode.plugin.zsh
+              ''
+          };
+        };
+      };
+    };
+
+    users.users.${cfg.name} = {
+      isNormalUser = true;
+
+      inherit (cfg) name initialPassword;
+
+      home = "/home/${cfg.name}";
+      group = "users";
+
+      shell = pkgs.zsh;
+
+      uid = 1000;
+
+      extraGroups = [ ] ++ cfg.extraGroups;
+    } // cfg.extraOptions;
+  };
+}
