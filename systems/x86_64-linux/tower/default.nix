907d2c41333f277a76a11d5cbc3687b51d82ab63
Christoph <christoph00@users.noreply.github.com>
2024-09-05 10:42:47 +0200

    ok

--- systems/x86_64-linux/tower/default.nix
+++ systems/x86_64-linux/tower/default.nix
@@ -14,24 +14,19 @@
 
   internal.type = "desktop";
   internal.isV3 = true;
+  internal.system.boot.secureBoot = false;
 
   environment.systemPackages = [pkgs.amdgpu_top];
   boot.kernelModules = ["kvm-intel" "acpi_call" "i2c_dev" "amdgpu"];
-  boot.blacklistedKernelModules = ["dm_mod"];
   boot.kernelParams = [
-    "quiet"
     "rcutree.rcu_idle_gp_delay=1"
-    "splash"
-    "loglevel=3"
-    "udev.log-priority=3"
-    "vt.global_cursor_default=0"
     "mem_sleep_default=deep"
     "amdgpu.gartsize=4096"
     "adgpu.ignore_crat=1"
   ];
 
   boot.initrd = {
-    availableKernelModules = ["xhci_pci" "ahci" "nvme" "usb_storage" "sd_mod"];
+    availableKernelModules = ["xhci_pci" "ahci" "nvme"];
     kernelModules = ["amdgpu"];
   };
 
