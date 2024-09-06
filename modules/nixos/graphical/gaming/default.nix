0d0b374d4343c5c8fc97dbb74f6ebd671b33c06e
christoph00 <christoph@asche.co>
2024-09-06 09:17:29 +0200

    ok

--- modules/nixos/graphical/gaming/default.nix
+++ modules/nixos/graphical/gaming/default.nix
@@ -85,17 +85,17 @@ in
       applications = {
 
         apps = [
-           {
-          name = "1080p Desktop";
-          prep-cmd = [
-            {
-              do = "${pkgs.kdePackages.libkscreen}/bin/kscreen-doctor output.DP-2.disable output.HDMI-A-1.mode.1920x1080@60 output.HDMI-A-1.enable";
-              undo = "${pkgs.kdePackages.libkscreen}/bin/kscreen-doctor output.HDMI-A-1.disable output.DP-2.mode.1 output.DP-2.enable ";
-            }
-          ];
-          exclude-global-prep-cmd = "false";
-          auto-detach = "true";
-        }
+          {
+            name = "1080p Desktop";
+            prep-cmd = [
+              {
+                do = "${pkgs.kdePackages.libkscreen}/bin/kscreen-doctor output.DP-2.disable output.HDMI-A-1.mode.1920x1080@60 output.HDMI-A-1.enable";
+                undo = "${pkgs.kdePackages.libkscreen}/bin/kscreen-doctor output.HDMI-A-1.disable output.DP-2.mode.1 output.DP-2.enable ";
+              }
+            ];
+            exclude-global-prep-cmd = "false";
+            auto-detach = "true";
+          }
         ];
       };
     };
