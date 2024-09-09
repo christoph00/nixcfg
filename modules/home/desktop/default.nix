0bbe976a19d1dba84e1ee8903561c25a38eebebf
Christoph <christoph00@users.noreply.github.com>
2024-09-09 10:01:54 +0200

    ok

--- modules/home/desktop/default.nix
+++ modules/home/desktop/default.nix
@@ -17,6 +17,7 @@ let
     ;
   inherit (lib.internal) mkBoolOpt;
   cfg = config.profiles.internal.desktop;
+  
 in
 {
   options.profiles.internal.desktop = with types; {
@@ -27,32 +28,6 @@ in
     fonts.fontconfig.enable = true;
 
 
-    systemd.user.services.wayvnc = {
-      Unit = {
-        Description = "VNC Server";
-        # Allow it to restart infinitely
-        StartLimitIntervalSec = 0;
-      };
-
-      Service = {
-        ExecStart = "${pkgs.writeShellScript "wayvnc-start" ''
-          if [[ $XDG_SESSION_TYPE = "wayland" ]]; then
-            ${lib.getExe pkgs.wayvnc} && exit 1
-          else
-            exit 0
-          fi
-        ''}";
-        Restart = "on-failure";
-        RestartSec = "1m";
-      };
-
-      Install = { WantedBy = [ "graphical-session.target" ]; };
-    };
-
-    # As we don't open the firewall, it should only be accessible over Tailscale
-    xdg.configFile."wayvnc/config".text = ''
-      address=0.0.0.0
-    '';
   };
 
 }
