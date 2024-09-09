c8b4e021f90b23be785ef21e56acadc96846e74b
christoph00 <christoph@asche.co>
2024-09-09 20:06:39 +0200

    ok

--- modules/nixos/graphical/desktop/headless/default.nix
+++ modules/nixos/graphical/desktop/headless/default.nix
@@ -44,8 +44,8 @@ in
 
     boot.kernelModules = [ "uinput" ];
     services.udev.extraRules = ''
-        KERNEL=="uinput", GROUP="input", MODE="0660" OPTIONS+="static_node=uinput"
-        '';
+      KERNEL=="uinput", GROUP="input", MODE="0660" OPTIONS+="static_node=uinput"
+    '';
 
     environment.sessionVariables = {
       WLR_BACKENDS = "drm,headless,libinput";
