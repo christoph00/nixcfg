9dfe45455feaad8d0841e894f05f6d2cad8f7cb8
Christoph <christoph00@users.noreply.github.com>
2024-09-09 12:33:36 +0200

    ok

--- modules/home/desktop/headless/default.nix
+++ modules/home/desktop/headless/default.nix
@@ -24,9 +24,58 @@ in
   options.profiles.internal.desktop.headless = with types; {
     enable = mkBoolOpt false "Enable Headless Desktop";
     autorun = mkBoolOpt true "Auto Start Service";
+    vnc {
+      addr = mkOption {
+        type = types.str;
+        default = "0.0.0.0";
+        description = ''
+          Which address should wayvnc listen to.
+        '';
+      };
+      maxFps = mkOption {
+        type = types.int;
+        default = 30;
+        description = ''
+          Set the rate limit.
+        '';
+      };
+      port = mkOption {
+        type = types.int;
+        default = 5900;
+        description = ''
+          Set the port to listen on.
+        '';
+      };
+    }
   };
 
   config = mkIf cfg.enable {
+
+    home.packages = [pkgs.wayvnc];
+
+    xdg.configFile."wayvnc/config".text = ''
+      port=${toString cfg.vnc.port}
+    '';
+
+    systemd.user.services.wayvnc = {
+      Unit = {
+        Description = "a VNC server for wlroots based Wayland compositors";
+        After = ["graphical-session-pre.target"];
+        PartOf = ["graphical-session.target"];
+      };
+
+      Service = {
+        Restart = "on-failure";
+        ExecStart = ''          ${pkgs.wayvnc}/bin/wayvnc \
+                    -f ${assert asserts.assertMsg (cfg.vnc.maxFps > 0) "Rate limit for WayVNC must be a positive integer!"; toString maxFps} \
+                    ${cfg.vnc.addr}
+        '';
+      };
+
+      Install = {
+        WantedBy = ["graphical-session.target"];
+      };
+    };
 
     systemd.user.services.headless-desktop = {
       Unit = {
