c43e018fecc040f1b85118ef5ac150263dc34ae6
christoph00 <christoph@asche.co>
2024-09-09 21:59:08 +0200

    ok

---
+++ modules/home/theme/theme.nix
@@ -0,0 +1,106 @@
+{
+  options,
+  config,
+  lib,
+  pkgs,
+  namespace,
+  ...
+}:
+
+let
+  inherit (lib)
+    types
+    listOf
+    mkIf
+    mkMerge
+    mkDefault
+    mkOption
+    ;
+  inherit (lib.internal) mkBoolOpt;
+  cfg = config.profiles.internal.theme;
+
+  nerdfonts = pkgs.nerdfonts.override {
+    fonts = [
+      "Ubuntu"
+      "UbuntuMono"
+      "CascadiaCode"
+      "FantasqueSansMono"
+      "FiraCode"
+      "Mononoki"
+    ];
+  };
+
+  theme = {
+    name = "adw-gtk3-dark";
+    package = pkgs.adw-gtk3;
+  };
+  font = {
+    name = "Ubuntu Nerd Font";
+    package = nerdfonts;
+    size = 11;
+  };
+  cursorTheme = {
+    name = "Qogir";
+    size = 24;
+    package = pkgs.qogir-icon-theme;
+  };
+  iconTheme = {
+    name = "MoreWaita";
+    package = pkgs.morewaita-icon-theme;
+  };
+in
+{
+  options.profiles.internal.theme = with types; {
+    enable = mkBoolOpt config.profiles.internal.desktop.enable "Enable Theme Options";
+  };
+  config = mkIf cfg.enable {
+    home = {
+      packages = with pkgs; [
+        cantarell-fonts
+        font-awesome
+        theme.package
+        font.package
+        cursorTheme.package
+        iconTheme.package
+        adwaita-icon-theme
+        papirus-icon-theme
+      ];
+      sessionVariables = {
+        XCURSOR_THEME = cursorTheme.name;
+        XCURSOR_SIZE = "${toString cursorTheme.size}";
+      };
+      pointerCursor = cursorTheme // {
+        gtk.enable = true;
+      };
+      file = {
+        ".config/gtk-4.0/gtk.css".text = ''
+          window.messagedialog .response-area > button,
+          window.dialog.message .dialog-action-area > button,
+          .background.csd{
+            border-radius: 0;
+          }
+        '';
+      };
+    };
+
+    fonts.fontconfig.enable = true;
+
+    gtk = {
+      inherit font cursorTheme iconTheme;
+      theme.name = theme.name;
+      enable = true;
+      gtk3.extraCss = ''
+        headerbar, .titlebar,
+        .csd:not(.popup):not(tooltip):not(messagedialog) decoration{
+          border-radius: 0;
+        }
+      '';
+    };
+
+    qt = {
+      enable = true;
+      platformTheme.name = "kde";
+    };
+  };
+
+}
