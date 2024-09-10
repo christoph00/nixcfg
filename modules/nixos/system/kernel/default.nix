61554d17f683831eaa9957afdf478cf16a1b1c90
christoph00 <christoph@asche.co>
2024-09-10 20:28:43 +0200

    ok

--- modules/nixos/system/kernel/default.nix
+++ modules/nixos/system/kernel/default.nix
@@ -73,7 +73,12 @@ in
           "net.ipv4.tcp_fastopen" = 3;
           # Bufferbloat mitigations + slight improvement in throughput & latency
           "net.ipv4.tcp_congestion_control" = "bbr";
-          "net.core.default_qdisc" = "cake";
+
+          "net.core.default_qdisc" = "fq";
+          "net.core.wmem_max" = 1073741824;
+          "net.core.rmem_max" = 1073741824;
+          "net.ipv4.tcp_rmem" = "4096 87380 1073741824";
+          "net.ipv4.tcp_wmem" = "4096 87380 1073741824";
         };
         boot.kernelModules = [ "tcp_bbr" ];
       }
