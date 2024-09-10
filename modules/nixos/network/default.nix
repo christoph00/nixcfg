6075c66bb2eb7411d4404962af2adf1cc5ed2ae2
christoph00 <christoph@asche.co>
2024-09-10 20:18:12 +0200

    ok

--- modules/nixos/network/default.nix
+++ modules/nixos/network/default.nix
@@ -22,6 +22,19 @@ in
   config = (mkIf cfg.enable) {
     networking.networkmanager.enable = mkDefault true;
     networking.wireless.enable = mkDefault false;
+
+    networking.wireless.iwd.settings = mkIf config.internal.isLaptop {
+      IPv6 = {
+        Enabled = true;
+      };
+      Settings = {
+        AutoConnect = true;
+      };
+    };
+
+    networking.networkmanager.wifi.backend = "iwd";
+
+
     systemd.services.NetworkManager-wait-online = {
       serviceConfig = {
         ExecStart = [
