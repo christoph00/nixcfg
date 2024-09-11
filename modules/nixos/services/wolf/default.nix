cb53023f69ed03b6147263df183cff06cd050280
christoph00 <christoph@asche.co>
2024-09-11 20:42:42 +0200

    ok

--- modules/nixos/services/wolf/default.nix
+++ modules/nixos/services/wolf/default.nix
@@ -74,11 +74,11 @@ in
       ];
       environment = {
         WOLF_LOG_LEVEL = "INFO";
-        HOST_APPS_STATE_FOLDER = "/data/wolf";
-        XDG_RUNTIME_DIR = "/mnt/state/wolf/sockets";
-        WOLF_CFG_FILE = "/mnt/state/wolf/cfg/config.toml";
-        WOLF_PRIVATE_KEY_FILE = "/mnt/state/wolf/cfg/key.pem";
-        WOLF_PRIVATE_CERT_FILE = "/mnt/state/wolf/cfg/cert.pem";
+        HOST_APPS_STATE_FOLDER = "/mnt/state/wolf";
+        #XDG_RUNTIME_DIR = "/mnt/state/wolf/sockets";
+        #WOLF_CFG_FILE = "/mnt/state/wolf/cfg/config.toml";
+        #WOLF_PRIVATE_KEY_FILE = "/mnt/state/wolf/cfg/key.pem";
+        #WOLF_PRIVATE_CERT_FILE = "/mnt/state/wolf/cfg/cert.pem";
         # TODO: Restore when Podman works
         # WOLF_DOCKER_SOCKET = "/run/podman/podman.sock";
         WOLF_DOCKER_SOCKET = "/var/run/docker.sock";
