62f799e0daa781be52b2150e2b0b67d6cc195b37
christoph00 <christoph@asche.co>
2024-09-11 21:06:51 +0200

    ok

--- modules/nixos/services/wolf/default.nix
+++ modules/nixos/services/wolf/default.nix
@@ -37,6 +37,16 @@ in
 
   config = mkIf cfg.enable {
 
+  security.rtkit.enable = true;
+  services.pipewire = {
+  enable = true;
+  alsa.enable = true;
+  alsa.support32Bit = true;
+  pulse.enable = true;
+  # If you want to use JACK applications, uncomment this
+  #jack.enable = true;
+};
+
     users.users = {
       wolf = {
         isNormalUser = true;
