3123f6d5a5fff10d0c86ea1d8cf108f275e5e1f1
christoph00 <christoph@asche.co>
2024-09-16 21:28:24 +0200

    ok

--- modules/home/desktop/headless/default.nix
+++ modules/home/desktop/headless/default.nix
@@ -4,7 +4,6 @@
   lib,
   pkgs,
   namespace,
-  inputs,
   ...
 }:
 let
@@ -17,9 +16,7 @@ let
     mkOption
     optional
     asserts
-    getExe
     ;
-
   inherit (lib.internal) mkBoolOpt;
   cfg = config.profiles.internal.desktop.headless;
 
@@ -56,7 +53,78 @@ in
   config = mkIf cfg.enable {
 
     home.packages = [
+      pkgs.wayvnc
       pkgs.wlr-randr
     ];
+
+    xdg.configFile."wayvnc/config".text = ''
+      port=${toString cfg.vnc.port}
+    '';
+
+     profiles.internal.desktop.wayfire.settings = {
+      plugins = [
+        {
+          plugin = "output:HEADLESS-1";
+          settings.mode = "1920x1080@60000";
+        }
+        {
+          plugin = "output:HDMI-A-1";
+          settings.mode = "off";
+        }
+        {
+          plugin = "output:DP-1";
+          settings.mode = "off";
+        }
+      ];
+    };
+
+
+
+
+
+    systemd.user.services.wayvnc = {
+      Unit = {
+        Description = "a VNC server for wlroots based Wayland compositors";
+        After = [ "graphical-session-pre.target" ];
+        # PartOf = [ "graphical-session.target" ];
+      };
+
+      Service = {
+        Restart = "on-failure";
+        ExecStart = ''
+          ${pkgs.wayvnc}/bin/wayvnc -g \
+                    -f ${
+                      assert asserts.assertMsg (cfg.vnc.maxFps > 0) "Rate limit for WayVNC must be a positive integer!";
+                      toString cfg.vnc.maxFps
+                    } \
+                    ${cfg.vnc.addr}
+        '';
+      };
+
+      # Install = {
+      #   WantedBy = [ "graphical-session.target" ];
+      # };
+    };
+
+    systemd.user.services.headless-desktop = {
+      Unit = {
+        Description = "Headless Desktop";
+        PartOf = [ "graphical-session.target" ];
+        After = [ "graphical-session-pre.target" ];
+
+      };
+      Install.WantedBy = [
+        (lib.mkIf cfg.autorun "default.target")
+      ];
+
+      #environment.PATH = lib.mkForce null;
+      Service = {
+        Type = "simple";
+        #ExecStartPre =  "${pkgs.dbus}/bin/dbus-update-activation-environment --systemd DISPLAY WAYLAND_DISPLAY XDG_CURRENT_DESKTOP; systemctl --user import-environment";
+        ExecStart = "${pkgs.runtimeShell} -c 'source /etc/set-environment; exec ${config.profiles.internal.desktop.wayfire.finalPackage}/bin/wayfire'";
+
+      };
+
+    };
   };
 }
