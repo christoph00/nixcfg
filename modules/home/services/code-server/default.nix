3a50382ff383deb8d3267908446c3fa584020ce2
Christoph <christoph00@users.noreply.github.com>
2024-09-17 08:31:48 +0200

    ok

--- modules/home/services/code-server/default.nix
+++ modules/home/services/code-server/default.nix
@@ -37,15 +37,6 @@ in
   # enable Wayland support
   home.sessionVariables.NIXOS_OZONE_WL = "1";
 
-  # enable vscode-server
-  imports = [
-    "${
-      fetchTarball {
-        url = "https://github.com/msteen/nixos-vscode-server/tarball/master";
-        sha256 = "1rq8mrlmbzpcbv9ys0x88alw30ks70jlmvnfr2j8v830yy5wvw7h";
-      }
-    }/modules/vscode-server/home.nix"
-  ];
   services.vscode-server.enable = true;
   services.vscode-server.enableFHS = true;
   services.vscode-server.installPath = "$HOME/.vscode";
@@ -68,7 +59,7 @@ in
       RestartSec = 10;
     };
     Install = {
-      WantedBy = ["basic.target"];
+      WantedBy = ["default.target"];
     };
   };
   };
