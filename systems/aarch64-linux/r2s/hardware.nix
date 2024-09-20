010552ee548ab646362ea1a34d589aa1955559b3
Christoph <christoph00@users.noreply.github.com>
2024-09-20 07:59:11 +0200

    ok

---
+++ systems/aarch64-linux/r2s/hardware.nix
@@ -0,0 +1,109 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}: {
+  hardware.deviceTree.name = "rockchip/rk3328-nanopi-r2s.dtb";
+
+
+  # NanoPi R2S's DTS has not been actively updated, so just use the prebuilt one to avoid rebuilding
+  hardware.deviceTree.package = pkgs.lib.mkForce (
+    pkgs.runCommand "dtbs-nanopi-r2s" {} ''
+      install -TDm644 ${./files/rk3328-nanopi-r2s.dtb} $out/rockchip/rk3328-nanopi-r2s.dtb
+    ''
+  );
+
+  hardware.firmware = [
+    (
+      pkgs.runCommand
+      "linux-firmware-r8152"
+      {}
+      ''
+        install -TDm644 ${./files/rtl8153a-4.fw} $out/lib/firmware/rtl_nic/rtl8153a-4.fw
+        install -TDm644 ${./files/rtl8153b-2.fw} $out/lib/firmware/rtl_nic/rtl8153b-2.fw
+      ''
+    )
+  ];
+
+  fileSystems = {
+     "/boot" = {
+       device = "/dev/disk/by-label/NIXOS_BOOT";
+       fsType = "ext4";
+     };
+     "/" = {
+       device = "/dev/disk/by-label/NIXOS_SD";
+       fsType = "f2fs";
+       options = ["compress_algorithm=zstd:6" "compress_chksum" "atgc" "gc_merge" "lazytime"];
+     };
+
+  };
+
+  boot = {
+    loader = {
+      timeout = 1;
+      grub.enable = false;
+      generic-extlinux-compatible = {
+        enable = true;
+        configurationLimit = 5;
+      };
+    };
+    kernelPackages = pkgs.linuxPackages;
+    kernelParams = [
+      "console=ttyS2,1500000"
+      "earlycon=uart8250,mmio32,0xff130000"
+      "mitigations=off"
+    ];
+    initrd = {
+      includeDefaultModules = false;
+      kernelModules = ["ledtrig-netdev"];
+    };
+    blacklistedKernelModules = ["hantro_vpu" "drm" "lima" "videodev"];
+    kernelModules = ["ledtrig-netdev"];
+    tmp.useTmpfs = true;
+  };
+
+  boot.kernel.sysctl = {
+    "vm.vfs_cache_pressure" = 10;
+    "vm.dirty_ratio" = 50;
+    "vm.swappiness" = 20;
+  };
+
+  powerManagement.cpuFreqGovernor = "schedutil";
+
+
+  systemd.services."wait-system-running" = {
+    description = "Wait system running";
+    serviceConfig = {Type = "simple";};
+    script = ''
+      systemctl is-system-running --wait
+    '';
+  };
+
+  systemd.services."setup-net-leds" = {
+    description = "Setup network LEDs";
+    unitConfig = {DefaultDependencies = "no";};
+    serviceConfig = {Type = "simple";};
+    wantedBy = ["sysinit.target"];
+    script = ''
+      cd /sys/class/leds/nanopi-r2s:green:lan
+      echo netdev > trigger
+      echo 1 | tee link tx rx >/dev/null
+      echo intern0 > device_name
+
+      cd /sys/class/leds/nanopi-r2s:green:wan
+      echo netdev > trigger
+      echo 1 | tee link tx rx >/dev/null
+      echo extern0 > device_name
+    '';
+  };
+  systemd.services."setup-sys-led" = {
+    description = "Setup booted LED";
+    requires = ["wait-system-running.service"];
+    after = ["wait-system-running.service"];
+    wantedBy = ["multi-user.target"];
+    script = ''
+      echo default-on > /sys/class/leds/nanopi-r2s:red:sys/trigger
+    '';
+  };
+}
---
+++ systems/aarch64-linux/r2s/sd-image.nix
@@ -0,0 +1,20 @@
+{...}: {
+  imports = [
+    <nixpkgs/nixos/modules/installer/sd-card/sd-image-aarch64.nix>
+    ./hardware.nix
+  ];
+
+  services.openssh = {
+    enable = true;
+    permitRootLogin = "yes";
+  };
+
+  networking.useDHCP = true;
+    networking.hostName = "r2s";
+
+
+  users.extraUsers.root.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAHqEQOgEdi3e8uPWqE2nqzyiKC9Y792C5tNKco6lz4o christoph@tower"
+
+  ];
+}
