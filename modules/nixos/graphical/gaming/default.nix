2aff99ebd75f51a6a6b60e4c8b0c8a50d036ebf2
christoph00 <christoph@asche.co>
2024-09-25 20:33:40 +0200

    ok

--- modules/nixos/graphical/gaming/default.nix
+++ modules/nixos/graphical/gaming/default.nix
@@ -40,7 +40,35 @@ in
     environment.systemPackages = [
       pkgs.heroic
       pkgs.sunshine
-      pkgs.bottles
+      pkgs.lutris
+      (pkgs.bottles.override {
+        extraLibraries =
+          pkgs: with pkgs; [
+            giflib
+            libpng
+            gnutls
+            mpg123
+            openal
+            v4l-utils
+            libpulseaudio
+            libgpg-error
+            alsa-plugins
+            alsa-lib
+            libjpeg
+            sqlite
+            xorg.libXcomposite
+            xorg.libXinerama
+            libgcrypt
+            ncurses
+            ocl-icd
+            libxslt
+            libva
+            gtk3
+            jansson
+            vulkan-loader
+          ];
+      })
+
     ];
 
     programs = {
