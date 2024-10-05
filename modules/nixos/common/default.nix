90a89180965870f1600066010ce4ac36f61d2581
christoph00 <christoph@asche.co>
2024-10-05 14:34:22 +0200

    pk

--- modules/nixos/common/default.nix
+++ modules/nixos/common/default.nix
@@ -1,24 +1,28 @@
 {
   # Snowfall Lib provides a customized `lib` instance with access to your flake's library
   # as well as the libraries available from your flake's inputs.
-  lib,
-  # An instance of `pkgs` with your overlays and packages applied is also available.
-  pkgs,
-  # You also have access to your flake's inputs.
-  inputs,
-
-  # Additional metadata is provided by Snowfall Lib.
-  namespace, # The namespace used for your flake, defaulting to "internal" if not set.
-  system, # The system architecture for this host (eg. `x86_64-linux`).
-  target, # The Snowfall Lib target for this system (eg. `x86_64-iso`).
-  format, # A normalized name for the system target (eg. `iso`).
-  virtual, # A boolean to determine whether this system is a virtual target using nixos-generators.
-  systems, # An attribute map of your defined hosts.
+  lib
+, # An instance of `pkgs` with your overlays and packages applied is also available.
+  pkgs
+, # You also have access to your flake's inputs.
+  inputs
+, # Additional metadata is provided by Snowfall Lib.
+  namespace
+, # The namespace used for your flake, defaulting to "internal" if not set.
+  system
+, # The system architecture for this host (eg. `x86_64-linux`).
+  target
+, # The Snowfall Lib target for this system (eg. `x86_64-iso`).
+  format
+, # A normalized name for the system target (eg. `iso`).
+  virtual
+, # A boolean to determine whether this system is a virtual target using nixos-generators.
+  systems
+, # An attribute map of your defined hosts.
 
   # All other arguments come from the module system.
-  config,
-
-  ...
+  config
+, ...
 }:
 
 with builtins;
@@ -29,37 +33,32 @@ with lib.internal;
 
   config = {
 
-    # enable realtime capabilities to user processes
-    security.rtkit.enable = true;
-
     hardware.enableRedistributableFirmware = true;
+    security = {
 
-    security.sudo = {
-      enable = false;
-    };
-    security.doas = {
-      enable = true;
-      wheelNeedsPassword = false;
-      extraRules = [
-        {
-          groups = [ "wheel" ];
-          noPass = true;
-          keepEnv = true;
-        }
-      ];
+      # enable realtime capabilities to user processes
+      rtkit.enable = true;
+
+
+      sudo.enable = false;
+
+      doas = {
+        enable = true;
+        wheelNeedsPassword = false;
+        extraRules = [
+          {
+            groups = [ "wheel" ];
+            noPass = true;
+            keepEnv = true;
+          }
+        ];
+      };
     };
 
     console = {
       earlySetup = true;
       useXkbConfig = true;
     };
-
-    #environment.memoryAllocator.provider = "mimalloc";
-    #nixpkgs.overlays = [ (_: prev: { dhcpcd = prev.dhcpcd.override { enablePrivSep = false; }; }) ];
-
-    ## perlless
-    # Remove perl from activation
-    boot.initrd.systemd.enable = lib.mkDefault true;
     #system.etc.overlay.enable = lib.mkDefault true;
     services.userborn.enable = lib.mkDefault true;
 
@@ -67,8 +66,14 @@ with lib.internal;
     system.disableInstallerTools = lib.mkDefault true;
     programs.less.lessopen = lib.mkDefault null;
     programs.command-not-found.enable = lib.mkDefault false;
-    boot.enableContainers = lib.mkDefault false;
-    boot.loader.grub.enable = lib.mkDefault false;
+    boot = {
+
+      #environment.memoryAllocator.provider = "mimalloc";
+      #nixpkgs.overlays = [ (_: prev: { dhcpcd = prev.dhcpcd.override { enablePrivSep = false; }; }) ];
+      initrd.systemd.enable = lib.mkDefault true;
+      enableContainers = lib.mkDefault false;
+      loader.grub.enable = lib.mkDefault false;
+    };
     environment.defaultPackages = lib.mkDefault [ ];
     documentation.info.enable = lib.mkDefault false;
 
