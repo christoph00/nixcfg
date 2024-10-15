bc9e9ac1d22e57ad4580c304d8e0d5391e9c00c0
christoph00 <christoph@asche.co>
2024-10-15 07:34:06 +0200

    ok

--- modules/nixos/shell/default.nix
+++ modules/nixos/shell/default.nix
@@ -16,6 +16,42 @@ let
       {
         wrappers = {
           git = { basePackage = pkgs.git; };
+          starship = {
+            basePackage = pkgs.starship;
+            env.STARSHIP_CONFIG.value = (pkgs.formats.toml { }).generate "starship-config" {
+              character = {
+                success_symbol = "[☁ ](bold purple)";
+                error_symbol = "[☁ ](bold red)";
+                vicmd_symbol = "[☁ ](bold green)";
+              };
+            };
+          };
+          helix = {
+            basePackage = pkgs.helix;
+            flags = [
+              "-c"
+              (pkgs.writers.writeTOML "helix-config" {
+                theme = "modus_vivendi";
+                editor = {
+                  line-number = "relative";
+                  cursorline = true;
+                  true-color = true;
+                  color-modes = true;
+                  bufferline = "multiple";
+                };
+                editor.cursor-shape = {
+                  insert = "underline";
+                  normal = "bar";
+                };
+                editor.statusline = {
+                  left = [ "mode" "spinner" "read-only-indicator" "file-modification-indicator" ];
+                  center = [ "file-name" ];
+                };
+                editor.indent-guides.render = true;
+                editor.soft-wrap.enable = true;
+              })
+            ];
+          };
         };
       }
     ];
