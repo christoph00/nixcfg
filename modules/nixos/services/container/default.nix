9cb2792694207bdccc69523aa606889f41279328
christoph00 <christoph@asche.co>
2024-10-18 09:21:42 +0200

    ok

---
+++ modules/nixos/services/container/default.nix
@@ -0,0 +1,70 @@
+{
+  # Snowfall Lib provides a customized `lib` instance with access to your flake's library
+  # as well as the libraries available from your flake's inputs.
+  lib,
+  # An instance of `pkgs` with your overlays and packages applied is also available.
+  pkgs,
+  # You also have access to your flake's inputs.
+  inputs,
+
+  # Additional metadata is provided by Snowfall Lib.
+  namespace, # The namespace used for your flake, defaulting to "internal" if not set.
+  system, # The system architecture for this host (eg. `x86_64-linux`).
+  target, # The Snowfall Lib target for this system (eg. `x86_64-iso`).
+  format, # A normalized name for the system target (eg. `iso`).
+  virtual, # A boolean to determine whether this system is a virtual target using nixos-generators.
+  systems, # An attribute map of your defined hosts.
+
+  # All other arguments come from the module system.
+  config,
+
+  ...
+}:
+
+with builtins;
+with lib;
+with lib.internal;
+
+let
+  cfg = config.internal.services.container;
+
+in
+
+{
+
+  options.internal.services.container = {
+    enable = mkBoolOpt false "Enable Container Services.";
+  };
+
+  config = mkIf cfg.enable {
+
+    environment.systemPackages = with pkgs; [ podman-compose ];
+
+    virtualisation.podman.enable = true;
+    virtualisation.podman.dockerCompat = true;
+    virtualisation.podman.defaultNetwork.settings.dns_enabled = true;
+
+    virtualisation.podman.autoPrune.enable = true;
+    virtualisation.podman.autoPrune.dates = "weekly";
+    virtualisation.podman.autoPrune.flags = [ "--all" ];
+
+    systemd.services.podman-auto-update = {
+      wants = [ "network-online.target" ];
+      after = [ "network-online.target" ];
+      serviceConfig = {
+        Type = "oneshot";
+        ExecStart = "${pkgs.podman}/bin/podman auto-update";
+        ExecStartPost = "${pkgs.podman}/bin/podman image prune -f";
+      };
+    };
+
+    systemd.timers.podman-auto-update = {
+      wantedBy = [ "timers.target" ];
+      timerConfig = {
+        OnCalendar = "03:30";
+        Persistent = true;
+      };
+    };
+  };
+
+}
