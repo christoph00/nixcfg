18ddf3537ae6c40df8e378494bcd2ec8b236cf43
christoph00 <christoph@asche.co>
2024-10-18 08:58:58 +0200

    ok

--- systems/aarch64-linux/rpi/default.nix
+++ systems/aarch64-linux/rpi/default.nix
@@ -7,7 +7,7 @@
 , ...
 }:
 {
-  imports = [ inputs.raspberry-pi-nix.nixosModules.raspberry-pi ];
+  imports = [ inputs.raspberry-pi-nix.nixosModules.raspberry-pi (modulesPath + "/installer/scan/not-detected.nix") ];
 
   networking.hostName = "rpi";
 
@@ -15,6 +15,28 @@
   internal.system.boot.enable = false;
   internal.system.fs.enable = false;
   internal.system.state.enable = false;
+
+
+  boot.initrd.availableKernelModules = [ "usb_storage" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ ];
+  boot.extraModulePackages = [ ];
+
+
+  boot.initrd.systemd.tpm2.enable = false;
+
+  fileSystems."/" = {
+    device = "/dev/disk/by-label/NIXOS_SD";
+    fsType = "ext4";
+  };
+
+  fileSystems."/boot/firmware" = {
+    device = "/dev/disk/by-label/FIRMWARE";
+    fsType = "vfat";
+    options = [ "fmask=0022" "dmask=0022" ];
+  };
+
+  swapDevices = [ ];
 
   raspberry-pi-nix = {
     board = "bcm2711";
