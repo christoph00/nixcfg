17394095df151e57a8cc1ede98ca6f4c90c08fc3
christoph00 <christoph@asche.co>
2024-10-21 14:00:48 +0200

    add oc2

---
+++ systems/x86_64-linux/oc2/default.nix
@@ -0,0 +1,26 @@
+{ config
+, lib
+, pkgs
+, modulesPath
+, inputs
+, namespace
+, ...
+}:
+{
+  imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
+
+  networking.hostName = "oc2";
+
+  internal.type = "vm";
+  internal.system.fs.device = "/dev/sda";
+
+  boot.initrd.kernelModules = [
+    "ata_piix"
+    "uhci_hcd"
+    "xen_blkfront"
+    "vmw_pvscsi"
+    "nvme"
+  ];
+
+  system.stateVersion = "24.05";
+}
