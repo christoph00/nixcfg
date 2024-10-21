d9d3f658b74e69199aa92dc2dd64993e311207f1
christoph00 <christoph@asche.co>
2024-10-21 13:59:32 +0200

    add lsrv

---
+++ systems/x86_64-linux/lsrv/default.nix
@@ -0,0 +1,38 @@
+##
+# Z170 Extreme4 P7.50
+# i7-6700k
+# RX580 Sapphire
+##
+{ config
+, lib
+, pkgs
+, modulesPath
+, inputs
+, namespace
+, ...
+}:
+{
+  imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
+
+  networking.hostName = "lsrv";
+
+  internal.type = "server";
+  internal.isV3 = true;
+  internal.system.boot.secureBoot = false;
+
+  boot.kernelModules = [
+    "kvm-intel"
+    "acpi_call"
+    "i2c_dev"
+  ];
+
+  boot.initrd = {
+    availableKernelModules = [
+      "xhci_pci"
+      "ahci"
+      "nvme"
+    ];
+  };
+
+  system.stateVersion = "24.05";
+}
