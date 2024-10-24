400728c6da12ae09dcd48384d24d90e70c70c298
christoph00 <christoph@asche.co>
2024-10-24 16:22:24 +0200

    ok

--- modules/nixos/graphical/desktop/display-manager/default.nix
+++ modules/nixos/graphical/desktop/display-manager/default.nix
@@ -1,27 +1,28 @@
 {
   # Snowfall Lib provides a customized `lib` instance with access to your flake's library
   # as well as the libraries available from your flake's inputs.
-  lib,
-  # An instance of `pkgs` with your overlays and packages applied is also available.
-  pkgs,
-  # You also have access to your flake's inputs.
-  inputs,
-  # Additional metadata is provided by Snowfall Lib.
-  namespace,
-  # The namespace used for your flake, defaulting to "internal" if not set.
-  system,
-  # The system architecture for this host (eg. `x86_64-linux`).
-  target,
-  # The Snowfall Lib target for this system (eg. `x86_64-iso`).
-  format,
-  # A normalized name for the system target (eg. `iso`).
-  virtual,
-  # A boolean to determine whether this system is a virtual target using nixos-generators.
-  systems, # An attribute map of your defined hosts.
+  lib
+, # An instance of `pkgs` with your overlays and packages applied is also available.
+  pkgs
+, # You also have access to your flake's inputs.
+  inputs
+, # Additional metadata is provided by Snowfall Lib.
+  namespace
+, # The namespace used for your flake, defaulting to "internal" if not set.
+  system
+, # The system architecture for this host (eg. `x86_64-linux`).
+  target
+, # The Snowfall Lib target for this system (eg. `x86_64-iso`).
+  format
+, # A normalized name for the system target (eg. `iso`).
+  virtual
+, # A boolean to determine whether this system is a virtual target using nixos-generators.
+  systems
+, # An attribute map of your defined hosts.
 
   # All other arguments come from the module system.
-  config,
-  ...
+  config
+, ...
 }:
 
 with builtins;
@@ -42,17 +43,32 @@ in
 
   config = mkIf cfg.enable {
     services = {
-      xserver.displayManager.startx.enable = true;
-      displayManager.sddm = {
-        enable = true;
-        wayland = {
-          enable = cfg.wayland;
-          compositor = "kwin";
+      xserver.enable = true;
+      xserver.displayManager = {
+        startx.enable = true;
+        lightdm = {
+          background = "#008080";
+          greeters.gtk = {
+            cursorTheme = {
+              package = pkgs.internal.chicago95;
+              name = "Chicago95_Animated_Hourglass_Cursors";
+            };
+            iconTheme = {
+              package = pkgs.internal.chicago95;
+              name = "Chicago95";
+            };
+            theme = {
+              package = pkgs.internal.chicago95;
+              name = "Chicago95";
+            };
+            extraConfig = ''
+              font-name=Helvetica 8
+            '';
+          };
         };
+
       };
 
     };
 
-  };
-
-}
+  }
