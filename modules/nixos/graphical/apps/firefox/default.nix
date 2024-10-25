8eb352f689686970efd606a2dc9d7dd22837fed9
christoph00 <christoph@asche.co>
2024-10-25 22:14:38 +0200

    ok

--- modules/nixos/graphical/apps/firefox/default.nix
+++ modules/nixos/graphical/apps/firefox/default.nix
@@ -1,13 +1,13 @@
 {
   # Snowfall Lib provides a customized `lib` instance with access to your flake's library
   # as well as the libraries available from your flake's inputs.
-  lib,
-  # An instance of `pkgs` with your overlays and packages applied is also available.
-  pkgs,
-  # You also have access to your flake's inputs.
-  config,
-  inputs,
-  ...
+  lib
+, # An instance of `pkgs` with your overlays and packages applied is also available.
+  pkgs
+, # You also have access to your flake's inputs.
+  config
+, inputs
+, ...
 }:
 
 with builtins;
@@ -26,57 +26,154 @@ in
 
   config = mkIf cfg.enable {
 
-    environment.systemPackages =
-      let
-        wrapped = inputs.wrapper-manager.lib.build {
-          inherit pkgs;
-          modules = [
-            {
-              wrappers = {
-                firefox = {
-                  basePackage = pkgs.wrapFirefox pkgs.firefox-beta-unwrapped {
-                    extraPolicies = {
-                      CaptivePortal = false;
-                      DisableFirefoxStudies = true;
-                      DisablePocket = true;
-                      DisableTelemetry = true;
-                      DisableFirefoxAccounts = true;
-                      FirefoxHome = {
-                        Pocket = false;
-                        Snippets = false;
-                      };
-                      UserMessaging = {
-                        ExtensionRecommendations = false;
-                        SkipOnboarding = true;
-                      };
-                      ExtensionSettings = import ./extensions.nix { inherit inputs pkgs lib; };
-                    };
-                    extraPrefs = import ./preferences.nix { inherit inputs; };
-                  };
+    programs.firefox = {
+      enable = true;
+      package = pkgs.firefox-beta-bin;
 
-                };
+      policies = {
+        AppAutoUpdate = false;
 
+        Containers.Default =
+          let
+            mkContainer = name: color: icon: {
+              inherit name color icon;
+            };
+          in
+          [
+            (mkContainer "Personal" "blue" "fingerprint")
+            (mkContainer "Self-hosted" "pink" "fingerprint")
+            (mkContainer "Shopping" "pink" "cart")
+            (mkContainer "Gaming" "turquoise" "chill")
+          ];
+
+        DisableAppUpdate = true;
+        DisableMasterPasswordCreation = true;
+        DisablePocket = true;
+        DisableSetDesktopBackground = true;
+        DontCheckDefaultBrowser = true;
+
+        ExtensionSettings =
+          let
+            mozillaAddon = id: "https://addons.mozilla.org/firefox/downloads/latest/${id}/latest.xpi";
+
+            # Unlike the user-specific browser configuration, we're just
+            # considering the bare minimum set of preferred extensions.
+            extensions = {
+              "@contain-google".install_url = mozillaAddon "google-container";
+              "@testpilot-containers".install_url = mozillaAddon "multi-account-containers";
+              "passbolt@passbolt.com" = {
+                install_url = mozillaAddon "passbolt";
+                installation_mode = "force_installed";
+                default_area = "navbar";
               };
+
+              "uBlock0@raymondhill.net".install_url = mozillaAddon "ublock-origin";
+
+              "de-DE@dictionaries.addons.mozilla.org".install_url = mozillaAddon "dictionary-german";
+
+            };
+
+            applyInstallationMode =
+              name: value:
+              lib.nameValuePair name (
+                value
+                // (lib.optionalAttrs (!(lib.hasAttrByPath [ "installation_mode" ] value)) {
+                  installation_mode = "normal_installed";
+                })
+              );
+          in
+          lib.mapAttrs' applyInstallationMode extensions;
+
+        FirefoxHome = {
+          Highlights = false;
+          Pocket = false;
+          Snippets = false;
+          SponsporedPocket = false;
+          SponsporedTopSites = false;
+        };
+        NoDefaultBookmarks = true;
+        OfferToSaveLoginsDefault = false;
+        PasswordManagerEnabled = false;
+        SanitizeOnShutdown = {
+          FormData = true;
+        };
+
+        SearchEngines = {
+          Add = [
+            {
+              Name = "Brave";
+              URLTemplate = "https://search.brave.com/search?q={searchTerms}";
+              Method = "GET";
+              IconURL = "https://brave.com/static-assets/images/brave-favicon.png";
+              Alias = "brave";
+              SuggestURLTemplate = "https://search.brave.com/api/suggest?q={searchTerms}";
+            }
+
+            {
+              Name = "nixpkgs";
+              URLTemplate = "https://search.nixos.org/packages?type=packages&query={searchTerms}";
+              Method = "GET";
+              Alias = "nixpkgs";
+              IconURL = "file://${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+            }
+
+            {
+              Name = "nix code";
+              URLTemplate = "https://github.com/search?q={searchTerms}+lang%3Anix&type=code";
+              Method = "GET";
+              Alias = "nix";
+              IconURL = "file://${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
             }
           ];
-        };
-      in
-      [
-        wrapped
-        (pkgs.makeDesktopItem {
-          exec = "${wrapped}/bin/firefox";
-
-          name = "firefox";
-          desktopName = "Firefox";
-          genericName = "Webbrowser";
-          icon = "firefox";
-          categories = [
-            "Network"
-            "WebBrowser"
+          Default = "Brave";
+          Remove = [
+            "Bing"
+            "Amazon"
+            "Wikipedia (en)"
           ];
-        })
-      ];
+        };
 
+        UseSystemPrintDialog = true;
+        UserMessaging = {
+          ExtensionRecommendations = false;
+          SkipOnboarding = true;
+        };
+      };
+
+
+      preferences = {
+        # Disable the UI tour.
+        "browser.uitour.enabled" = false;
+
+        # Don't tease me with the updates, man.
+        "apps.update.auto" = false;
+
+        # Some inconveniences of life (at least for me).
+        "extensions.pocket.enabled" = false;
+        "signon.rememberSignons" = false;
+
+        # Some quality of lifes.
+        "browser.search.widget.inNavBar" = true;
+        "browser.search.openintab" = true;
+
+        # Some privacy settings...
+        "privacy.donottrackheader.enabled" = true;
+        "privacy.trackingprotection.enabled" = true;
+
+        # Burn our own fingers.
+        "privacy.resistFingerprinting" = true;
+        "privacy.fingerprintingProtection" = true;
+        "privacy.fingerprintingProtection.pbmode" = true;
+
+        "privacy.query_stripping.enabled" = true;
+        "privacy.query_stripping.enabled.pbmode" = true;
+
+        "dom.security.https_first" = true;
+        "dom.security.https_first_pbm" = true;
+
+        "privacy.firstparty.isolate" = true;
+      };
+    };
   };
 
 }
