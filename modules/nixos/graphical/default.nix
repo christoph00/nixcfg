01d0864dfe8e6f91e608562f85e85e1a0e9c8737
christoph00 <christoph@asche.co>
2024-10-29 16:54:28 +0100

    ok

--- modules/nixos/graphical/default.nix
+++ modules/nixos/graphical/default.nix
@@ -1,27 +1,28 @@
 {
   # Snowfall Lib provides a customized `lib` instance with access to your flake's library
   # as well as the libraries available from your flake's inputs.
-  lib,
-  # An instance of `pkgs` with your overlays and packages applied is also available.
-  pkgs,
-  # You also have access to your flake's inputs.
-  inputs,
-  # Additional metadata is provided by Snowfall Lib.
-  namespace,
-  # The namespace used for your flake, defaulting to "internal" if not set.
-  system,
-  # The system architecture for this host (eg. `x86_64-linux`).
-  target,
-  # The Snowfall Lib target for this system (eg. `x86_64-iso`).
-  format,
-  # A normalized name for the system target (eg. `iso`).
-  virtual,
-  # A boolean to determine whether this system is a virtual target using nixos-generators.
-  systems, # An attribute map of your defined hosts.
+  lib
+, # An instance of `pkgs` with your overlays and packages applied is also available.
+  pkgs
+, # You also have access to your flake's inputs.
+  inputs
+, # Additional metadata is provided by Snowfall Lib.
+  namespace
+, # The namespace used for your flake, defaulting to "internal" if not set.
+  system
+, # The system architecture for this host (eg. `x86_64-linux`).
+  target
+, # The Snowfall Lib target for this system (eg. `x86_64-iso`).
+  format
+, # A normalized name for the system target (eg. `iso`).
+  virtual
+, # A boolean to determine whether this system is a virtual target using nixos-generators.
+  systems
+, # An attribute map of your defined hosts.
 
   # All other arguments come from the module system.
-  config,
-  ...
+  config
+, ...
 }:
 
 with builtins;
@@ -32,11 +33,11 @@ with lib.internal;
 
   config = mkIf config.internal.isGraphical {
 
-    internal.graphical.desktop.wayland.enable = false;
+    internal.graphical.desktop.wayland.enable = true;
     # internal.graphical.desktop.cosmic.enable = true;
-    # internal.graphical.desktop.wayfire.enable = true;
+    internal.graphical.desktop.wayfire.enable = true;
     # internal.graphical.desktop.plasma.enable = true;
-    internal.graphical.desktop.xfce.enable = true;
+    # internal.graphical.desktop.xfce.enable = true;
     hardware.graphics.enable = true;
 
     #programs.labwc.enable = true;
@@ -71,10 +72,6 @@ with lib.internal;
 
       phinger-cursors
 
-      kdePackages.qt6ct
-
-      catppuccin-qt5ct
-
       chicago95
 
     ];
@@ -85,11 +82,6 @@ with lib.internal;
 
     programs.dconf.enable = true;
     services = {
-
-      xserver = {
-        enable = true;
-        xkb.layout = "de";
-      };
 
       dbus.implementation = "broker";
 
