50fc91340913ba148ce2c1c4670a0e5e7d9e025e
christoph00 <christoph@asche.co>
2024-10-30 20:43:49 +0100

    ok

--- systems/x86_64-linux/vm-smarthome/default.nix
+++ systems/x86_64-linux/vm-smarthome/default.nix
@@ -1,21 +1,20 @@
-{
-  config,
-  lib,
-  pkgs,
-  modulesPath,
-  inputs,
-  namespace,
-  ...
+{ config
+, lib
+, pkgs
+, modulesPath
+, inputs
+, namespace
+, ...
 }:
 {
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
-    inputs.microvm.nixosModules.microvm
+    # inputs.microvm.nixosModules.microvm
   ];
 
   networking.hostName = "vm-smarthome";
 
-  internal.type = "microvm";
+  internal.type = "vm";
 
   system.stateVersion = "24.05";
 }
