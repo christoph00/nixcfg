0063d7299f50b1af3cac39a9ac4264291e17c0bb
christoph00 <christoph@asche.co>
2024-10-30 23:02:21 +0100

    ok

--- modules/nixos/graphical/apps/misc/default.nix
+++ modules/nixos/graphical/apps/misc/default.nix
@@ -1,13 +1,13 @@
 {
   # Snowfall Lib provides a customized `lib` instance with access to your flake's library
   # as well as the libraries available from your flake's inputs.
-  lib,
-  # An instance of `pkgs` with your overlays and packages applied is also available.
-  pkgs,
-  # You also have access to your flake's inputs.
-  config,
-  inputs,
-  ...
+  lib
+, # An instance of `pkgs` with your overlays and packages applied is also available.
+  pkgs
+, # You also have access to your flake's inputs.
+  config
+, inputs
+, ...
 }:
 
 with builtins;
@@ -51,6 +51,7 @@ in
       pkgs.zen-browser
       pkgs.zed-editor
       pkgs.vscode
+      pkgs.code-cursor
       pkgs.kitty
       pkgs.anyrun
       pkgs.moonlight-qt
