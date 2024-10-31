19685c5340bb612d88976315745ceff07620bca7
christoph00 <christoph@asche.co>
2024-10-31 07:38:09 +0100

    ok

---
+++ modules/nixos/graphical/apps/zed/default.nix
@@ -0,0 +1,44 @@
+{
+  # Snowfall Lib provides a customized `lib` instance with access to your flake's library
+  # as well as the libraries available from your flake's inputs.
+  lib
+, # An instance of `pkgs` with your overlays and packages applied is also available.
+  pkgs
+, # You also have access to your flake's inputs.
+  config
+, inputs
+, ...
+}:
+
+with builtins;
+with lib;
+with lib.internal;
+
+let
+  cfg = config.internal.graphical.apps.zed;
+
+  zed-fhs = pkgs.buildFHSUserEnv {
+      name = "zed";
+      targetPkgs = pkgs:
+        with pkgs; [
+          zed-editor
+        ];
+      runScript = "zed";
+    };
+in
+{
+
+  options.internal.graphical.apps.zed = {
+    enable = mkBoolOpt config.internal.isGraphical "Enable Zed.";
+  };
+
+  config = mkIf cfg.enable {
+
+  environment.systemPackages = with pkgs; [
+    zed-fhs
+    nixd
+    intelliphense
+    vscode-langservers-extracted
+  ];
+
+}
