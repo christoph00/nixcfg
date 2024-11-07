ed822f6d5939eb8e3899caa9d1a3df26883b968d
christoph00 <christoph@asche.co>
2024-11-07 08:37:29 +0100

    ok

--- modules/nixos/system/kernel/default.nix
+++ modules/nixos/system/kernel/default.nix
@@ -1,27 +1,28 @@
 {
   # Snowfall Lib provides a customized `lib` instance with access to your flake's library
   # as well as the libraries available from your flake's inputs.
-  lib,
-  # An instance of `pkgs` with your overlays and packages applied is also available.
-  pkgs,
-  # You also have access to your flake's inputs.
-  inputs,
-  # Additional metadata is provided by Snowfall Lib.
-  namespace,
-  # The namespace used for your flake, defaulting to "internal" if not set.
-  system,
-  # The system architecture for this host (eg. `x86_64-linux`).
-  target,
-  # The Snowfall Lib target for this system (eg. `x86_64-iso`).
-  format,
-  # A normalized name for the system target (eg. `iso`).
-  virtual,
-  # A boolean to determine whether this system is a virtual target using nixos-generators.
-  systems, # An attribute map of your defined hosts.
+  lib
+, # An instance of `pkgs` with your overlays and packages applied is also available.
+  pkgs
+, # You also have access to your flake's inputs.
+  inputs
+, # Additional metadata is provided by Snowfall Lib.
+  namespace
+, # The namespace used for your flake, defaulting to "internal" if not set.
+  system
+, # The system architecture for this host (eg. `x86_64-linux`).
+  target
+, # The Snowfall Lib target for this system (eg. `x86_64-iso`).
+  format
+, # A normalized name for the system target (eg. `iso`).
+  virtual
+, # A boolean to determine whether this system is a virtual target using nixos-generators.
+  systems
+, # An attribute map of your defined hosts.
 
   # All other arguments come from the module system.
-  config,
-  ...
+  config
+, ...
 }:
 
 with builtins;
@@ -158,9 +159,9 @@ in
         #boot.kernelPackages = pkgs.linuxPackages_latest;
         boot.kernelPackages = pkgs.linuxPackages_cachyos;
 
-        chaotic.scx.enable = true; # by default uses scx_rustland scheduler
-        chaotic.scx.scheduler = "scx_bpfland";
-        systemd.services.scx.serviceConfig.LogNamespace = "sched-ext";
+        # chaotic.scx.enable = true; # by default uses scx_rustland scheduler
+        # chaotic.scx.scheduler = "scx_bpfland";
+        # systemd.services.scx.serviceConfig.LogNamespace = "sched-ext";
         boot.kernelParams = [ "mitigations=off" ]; # disable mitigations on desktop
 
       })
