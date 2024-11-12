345ed90ef586e31917db14281cc2e45e57e76d0e
christoph00 <christoph@asche.co>
2024-11-12 10:02:08 +0100

    ok

--- modules/nixos/graphical/desktop/headless/default.nix
+++ modules/nixos/graphical/desktop/headless/default.nix
@@ -53,8 +53,8 @@ in
 
     systemd.user.services."wayland-wm-env@".environment = {
       WLR_BACKENDS = "drm,headless,libinput";
-      NIXOS_OZONE_WL = "1";
-      WAYLAND_DISPLAY = "wayland-1";
+      #NIXOS_OZONE_WL = "1";
+      #WAYLAND_DISPLAY = "wayland-1";
       #WLR_LIBINPUT_NO_DEVICES = "1";
       WLR_RENDERER = "pixman";
       WLR_RENDER_DRM_DEVICE = "/dev/dri/card0";
