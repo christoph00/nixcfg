3560755467f71425fdeb86426ebc71c36911d804
christoph00 <christoph@asche.co>
2024-11-14 16:57:39 +0100

    ok

--- modules/nixos/graphical/desktop/headless/default.nix
+++ modules/nixos/graphical/desktop/headless/default.nix
@@ -1,28 +1,27 @@
 {
   # Snowfall Lib provides a customized `lib` instance with access to your flake's library
   # as well as the libraries available from your flake's inputs.
-  lib
-, # An instance of `pkgs` with your overlays and packages applied is also available.
-  pkgs
-, # You also have access to your flake's inputs.
-  inputs
-, # Additional metadata is provided by Snowfall Lib.
-  namespace
-, # The namespace used for your flake, defaulting to "internal" if not set.
-  system
-, # The system architecture for this host (eg. `x86_64-linux`).
-  target
-, # The Snowfall Lib target for this system (eg. `x86_64-iso`).
-  format
-, # A normalized name for the system target (eg. `iso`).
-  virtual
-, # A boolean to determine whether this system is a virtual target using nixos-generators.
-  systems
-, # An attribute map of your defined hosts.
+  lib,
+  # An instance of `pkgs` with your overlays and packages applied is also available.
+  pkgs,
+  # You also have access to your flake's inputs.
+  inputs,
+  # Additional metadata is provided by Snowfall Lib.
+  namespace,
+  # The namespace used for your flake, defaulting to "internal" if not set.
+  system,
+  # The system architecture for this host (eg. `x86_64-linux`).
+  target,
+  # The Snowfall Lib target for this system (eg. `x86_64-iso`).
+  format,
+  # A normalized name for the system target (eg. `iso`).
+  virtual,
+  # A boolean to determine whether this system is a virtual target using nixos-generators.
+  systems, # An attribute map of your defined hosts.
 
   # All other arguments come from the module system.
-  config
-, ...
+  config,
+  ...
 }:
 
 with builtins;
@@ -31,18 +30,19 @@ with lib.internal;
 
 let
   cfg = config.internal.graphical.desktop.headless;
-  set_resolution =
-    (pkgs.writeShellScriptBin "set_resolution" ''
+  set_resolution = (
+    pkgs.writeShellScriptBin "set_resolution" ''
       if [ -z "$SUNSHINE_CLIENT_WIDTH" ] || [ -z "$SUNSHINE_CLIENT_HEIGHT" ] || [ -z "$SUNSHINE_CLIENT_FPS" ]; then
           echo "Missing env Vars from Sunshine"
           exit 1
       fi
 
       MODE="''${SUNSHINE_CLIENT_WIDTH}x''${SUNSHINE_CLIENT_HEIGHT}@''${SUNSHINE_CLIENT_FPS}Hz"
-      
+
       ${pkgs.wlr-randr}/bin/wlr-randr --output HEADLESS-1 --on --custom-mode $MODE
 
-    '');
+    ''
+  );
 in
 {
 
@@ -91,7 +91,6 @@ in
 
     services.seatd.enable = true;
 
-
     ## DP-2 = Monitor  HDMI-A-1 = Dummy
     # services.sunshine = {
     # enable = true;
@@ -114,8 +113,3 @@ in
   };
 
 }
-
-
-
-
-
