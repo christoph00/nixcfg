59384568182c71f60e6f5e72bb9812becf2f1fa2
christoph00 <christoph@asche.co>
2024-11-17 00:20:29 +0100

    ok

--- modules/nixos/services/homeassistant/default.nix
+++ modules/nixos/services/homeassistant/default.nix
@@ -45,8 +45,16 @@ in
       enable = true;
       openFirewall = true;
       package = pkgs.home-assistant;
+      extraComponents = [
+        # Components required to complete the onboarding
+        "esphome"
+        "met"
+        "radio_browser"
+      ];
       config = {
-
+        # Includes dependencies for a basic setup
+        # https://www.home-assistant.io/integrations/default_config/
+        default_config = { };
       };
     };
   };
