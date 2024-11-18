cf6708469069af057b5be2f98e194f6111e613bb
christoph00 <christoph@asche.co>
2024-11-18 11:12:46 +0100

    ok

--- modules/nixos/services/monitoring/default.nix
+++ modules/nixos/services/monitoring/default.nix
@@ -1,27 +1,28 @@
 {
   # Snowfall Lib provides a customized `lib` instance with access to your flake's library
   # as well as the libraries available from your flake's inputs.
-  lib,
-  # An instance of `pkgs` with your overlays and packages applied is also available.
-  pkgs,
-  # You also have access to your flake's inputs.
-  inputs,
-  # Additional metadata is provided by Snowfall Lib.
-  namespace,
-  # The namespace used for your flake, defaulting to "internal" if not set.
-  system,
-  # The system architecture for this host (eg. `x86_64-linux`).
-  target,
-  # The Snowfall Lib target for this system (eg. `x86_64-iso`).
-  format,
-  # A normalized name for the system target (eg. `iso`).
-  virtual,
-  # A boolean to determine whether this system is a virtual target using nixos-generators.
-  systems, # An attribute map of your defined hosts.
+  lib
+, # An instance of `pkgs` with your overlays and packages applied is also available.
+  pkgs
+, # You also have access to your flake's inputs.
+  inputs
+, # Additional metadata is provided by Snowfall Lib.
+  namespace
+, # The namespace used for your flake, defaulting to "internal" if not set.
+  system
+, # The system architecture for this host (eg. `x86_64-linux`).
+  target
+, # The Snowfall Lib target for this system (eg. `x86_64-iso`).
+  format
+, # A normalized name for the system target (eg. `iso`).
+  virtual
+, # A boolean to determine whether this system is a virtual target using nixos-generators.
+  systems
+, # An attribute map of your defined hosts.
 
   # All other arguments come from the module system.
-  config,
-  ...
+  config
+, ...
 }:
 
 with builtins;
@@ -36,8 +37,8 @@ in
 {
 
   options.internal.services.monitoring = {
-    enable = mkBoolOpt false "Enable Monitoring Service.";
-    hostMetrics = mkBoolOpt false "Enable Host Metrics";
+    enable = mkBoolOpt true "Enable Monitoring Service.";
+    hostMetrics = mkBoolOpt true "Enable Host Metrics";
     logFiles = lib.mkOption {
       default = [ ];
       type = lib.types.listOf lib.types.path;
