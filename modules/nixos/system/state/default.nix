bc9898c37ebae956e8685c99068d8a3e4b199b70
christoph00 <christoph@asche.co>
2024-11-19 22:20:55 +0100

    ok

--- modules/nixos/system/state/default.nix
+++ modules/nixos/system/state/default.nix
@@ -1,27 +1,28 @@
 {
   # Snowfall Lib provides a customized `lib` instance with access to your flake's library
   # as well as the libraries available from your flake's inputs.
-  lib,
-  # An instance of `pkgs` with your overlays and packages applied is also available.
-  pkgs,
-  # You also have access to your flake's inputs.
-  inputs,
-  # Additional metadata is provided by Snowfall Lib.
-  namespace,
-  # The namespace used for your flake, defaulting to "internal" if not set.
-  system,
-  # The system architecture for this host (eg. `x86_64-linux`).
-  target,
-  # The Snowfall Lib target for this system (eg. `x86_64-iso`).
-  format,
-  # A normalized name for the system target (eg. `iso`).
-  virtual,
-  # A boolean to determine whether this system is a virtual target using nixos-generators.
-  systems, # An attribute map of your defined hosts.
+  lib
+, # An instance of `pkgs` with your overlays and packages applied is also available.
+  pkgs
+, # You also have access to your flake's inputs.
+  inputs
+, # Additional metadata is provided by Snowfall Lib.
+  namespace
+, # The namespace used for your flake, defaulting to "internal" if not set.
+  system
+, # The system architecture for this host (eg. `x86_64-linux`).
+  target
+, # The Snowfall Lib target for this system (eg. `x86_64-iso`).
+  format
+, # A normalized name for the system target (eg. `iso`).
+  virtual
+, # A boolean to determine whether this system is a virtual target using nixos-generators.
+  systems
+, # An attribute map of your defined hosts.
 
   # All other arguments come from the module system.
-  config,
-  ...
+  config
+, ...
 }:
 
 # Necessary System State
@@ -61,6 +62,9 @@ in
     programs.fuse.userAllowOther = true;
     fileSystems."${cfg.stateDir}".neededForBoot = true;
 
+    boot.initrd.systemd.suppressedUnits = [ "systemd-machine-id-commit.service" ];
+    systemd.suppressedSystemUnits = [ "systemd-machine-id-commit.service" ];
+
     preservation = {
       enable = true;
       preserveAt."${cfg.stateDir}" = {
@@ -79,15 +83,15 @@ in
           }
         ] ++ cfg.files;
         directories = [
-        "/var/lib/bluetooth"
-        "/var/lib/fprint"
-        "/var/lib/fwupd"
-        "/var/lib/power-profiles-daemon"
-        "/var/lib/systemd/coredump"
-        "/var/lib/systemd/rfkill"
-        "/var/lib/systemd/timers"
-        { directory = "/var/lib/nixos"; inInitrd = true; }
-        { directory =  "/var/log"; inInitrd = true; }
+          "/var/lib/bluetooth"
+          "/var/lib/fprint"
+          "/var/lib/fwupd"
+          "/var/lib/power-profiles-daemon"
+          "/var/lib/systemd/coredump"
+          "/var/lib/systemd/rfkill"
+          "/var/lib/systemd/timers"
+          { directory = "/var/lib/nixos"; inInitrd = true; }
+          { directory = "/var/log"; inInitrd = true; }
         ] ++ cfg.directories;
       };
     };
