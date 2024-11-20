43e42dac8007933c6e302a2e2784d2cff5847e2e
christoph00 <christoph@asche.co>
2024-11-20 20:24:41 +0100

    ok

--- modules/nixos/services/homeassistant/default.nix
+++ modules/nixos/services/homeassistant/default.nix
@@ -60,9 +60,12 @@ in
     services.mosquitto = {
       enable = true;
       listeners = [{
-        acl = [ "pattern readwrite #" ];
-        omitPasswordAuth = true;
         settings.allow_anonymous = true;
+        acl = [ "topic readwrite #" ];
+        users = {
+          ha = { acl = [ "readwrite #" ]; };
+          robot = { acl = [ "readwrite #" ]; };
+        };
       }];
     };
 
