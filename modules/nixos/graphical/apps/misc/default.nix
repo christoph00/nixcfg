b888d5b4867e1c8b41bc3aa4fa8da58ea8b689fc
christoph00 <christoph@asche.co>
2024-11-21 14:34:01 +0100

    ok

--- modules/nixos/graphical/apps/misc/default.nix
+++ modules/nixos/graphical/apps/misc/default.nix
@@ -1,13 +1,13 @@
 {
   # Snowfall Lib provides a customized `lib` instance with access to your flake's library
   # as well as the libraries available from your flake's inputs.
-  lib,
-  # An instance of `pkgs` with your overlays and packages applied is also available.
-  pkgs,
-  # You also have access to your flake's inputs.
-  config,
-  inputs,
-  ...
+  lib
+, # An instance of `pkgs` with your overlays and packages applied is also available.
+  pkgs
+, # You also have access to your flake's inputs.
+  config
+, inputs
+, ...
 }:
 
 with builtins;
@@ -81,6 +81,10 @@ in
       pkgs.agenix
 
       pkgs.go-passbolt-cli
+
+      pkgs.pcmanfm
+
+      pkgs.nemo
 
       pkgs.internal.thorium-browser
     ];
