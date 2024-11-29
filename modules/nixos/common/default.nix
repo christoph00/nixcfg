565a8341dc26a2916aca5bcd428d1c0fef3749a1
christoph00 <christoph@asche.co>
2024-11-29 10:48:53 +0100

    ok

--- modules/nixos/common/default.nix
+++ modules/nixos/common/default.nix
@@ -1,27 +1,28 @@
 {
   # Snowfall Lib provides a customized `lib` instance with access to your flake's library
   # as well as the libraries available from your flake's inputs.
-  lib,
-  # An instance of `pkgs` with your overlays and packages applied is also available.
-  pkgs,
-  # You also have access to your flake's inputs.
-  inputs,
-  # Additional metadata is provided by Snowfall Lib.
-  namespace,
-  # The namespace used for your flake, defaulting to "internal" if not set.
-  system,
-  # The system architecture for this host (eg. `x86_64-linux`).
-  target,
-  # The Snowfall Lib target for this system (eg. `x86_64-iso`).
-  format,
-  # A normalized name for the system target (eg. `iso`).
-  virtual,
-  # A boolean to determine whether this system is a virtual target using nixos-generators.
-  systems, # An attribute map of your defined hosts.
+  lib
+, # An instance of `pkgs` with your overlays and packages applied is also available.
+  pkgs
+, # You also have access to your flake's inputs.
+  inputs
+, # Additional metadata is provided by Snowfall Lib.
+  namespace
+, # The namespace used for your flake, defaulting to "internal" if not set.
+  system
+, # The system architecture for this host (eg. `x86_64-linux`).
+  target
+, # The Snowfall Lib target for this system (eg. `x86_64-iso`).
+  format
+, # A normalized name for the system target (eg. `iso`).
+  virtual
+, # A boolean to determine whether this system is a virtual target using nixos-generators.
+  systems
+, # An attribute map of your defined hosts.
 
   # All other arguments come from the module system.
-  config,
-  ...
+  config
+, ...
 }:
 
 with builtins;
@@ -86,11 +87,10 @@ with lib.internal;
 
     # Re-add nixos-rebuild to the systemPackages that was removed by the
     # `system.disableInstallerTools` option.
-    #environment.systemPackages = [ pkgs.nixos-rebuild ];
+    # environment.systemPackages = [ pkgs.nixos-rebuild ];
 
-    environment.sessionVariables = {
-      FLAKEREF = "$HOME/Code/nixcfg";
-    };
+    environment.systemPackages = [ pkgs.internal.systemctl-mqtt ];
+
   };
 
 }
