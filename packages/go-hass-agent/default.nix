b3cd231ecb2955de0efb1a4519d5f957572219d6
christoph00 <christoph@asche.co>
2024-11-29 22:48:08 +0100

    ok

--- packages/go-hass-agent/default.nix
+++ packages/go-hass-agent/default.nix
@@ -1,14 +1,22 @@
-{
-  lib,
-  buildGoModule,
-  fetchFromGitHub,
+{ stdenv
+, lib
+, pkgs
+, buildGo123Module
+, # workaround till buildGoModule uses go 1.23 by default
+  pkg-config
+, glfw
+, xorg
+, mage
+, writeShellScriptBin
+, git
+, ...
 }:
 
-buildGoModule rec {
+buildGo123Module rec {
   pname = "go-hass-agent";
   version = "10.5.1";
 
-  src = fetchFromGitHub {
+  src = pkgs.fetchFromGitHub {
     owner = "joshuar";
     repo = "go-hass-agent";
     rev = "v${version}";
@@ -17,8 +25,54 @@ buildGoModule rec {
 
   vendorHash = "sha256-kzmnfHIPwS2S85t9bIdMnSMY83uY7ccj6UQF5VIT3Mc=";
 
-  ldflags = [ "-s" "-w" ];
+  doCheck = false;
 
+  nativeBuildInputs =
+    let
+      fakeGit = writeShellScriptBin "git" ''
+        if [[ $@ = "describe --tags --always --dirty" ]]; then
+            echo "${version}"
+        elif [[ $@ = "rev-parse --short HEAD" ]]; then
+            echo "dummyrev"
+        elif [[ $@ = "log --date=iso8601-strict -1 --pretty=%ct" ]]; then
+            echo "0"
+        else
+            ${git}/bin/git $@
+        fi
+      '';
+    in
+    [
+      fakeGit
+      pkg-config
+      mage
+    ];
+  buildInputs = with xorg; [
+    glfw
+    libX11
+    libXcursor
+    libXrandr
+    libXinerama
+    libXi
+    libXxf86vm
+  ];
+
+  buildPhase = ''
+    runHook preBuild
+
+    # Fixes “Error: error compiling magefiles” during build.
+    export HOME=$(mktemp -d)
+
+    mage -d build/magefiles -w . build:full
+
+    runHook postBuild
+  '';
+
+  installPhase = ''
+      runHook preInstall
+      mv dist/go-hass-agent-amd64 dist/go-hass-agent
+    install -Dt $out/bin dist/go-hass-agent
+    runHook postInstall
+  '';
   meta = {
     description = "A Home Assistant, native app for desktop/laptop devices";
     homepage = "https://github.com/joshuar/go-hass-agent";
