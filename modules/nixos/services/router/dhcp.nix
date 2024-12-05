fb24df2abea9681dd619de5ca740ea2962d5fcfa
christoph00 <christoph@asche.co>
2024-12-05 13:45:37 +0100

    ok

---
+++ modules/nixos/services/router/dhcp.nix
@@ -0,0 +1,143 @@
+{
+  # Snowfall Lib provides a customized `lib` instance with access to your flake's library
+  # as well as the libraries available from your flake's inputs.
+  lib
+, # An instance of `pkgs` with your overlays and packages applied is also available.
+  pkgs
+, # You also have access to your flake's inputs.
+  inputs
+, # Additional metadata is provided by Snowfall Lib.
+  namespace
+, # The namespace used for your flake, defaulting to "internal" if not set.
+  system
+, # The system architecture for this host (eg. `x86_64-linux`).
+  target
+, # The Snowfall Lib target for this system (eg. `x86_64-iso`).
+  format
+, # A normalized name for the system target (eg. `iso`).
+  virtual
+, # A boolean to determine whether this system is a virtual target using nixos-generators.
+  systems
+, # An attribute map of your defined hosts.
+
+  # All other arguments come from the module system.
+  config
+, ...
+}:
+
+with builtins;
+with lib;
+with lib.internal;
+
+let
+  cfg = config.internal.services.router.dhcp;
+
+in
+
+{
+
+   options.internal.services.router.dhcp = {
+    enable = mkBoolOpt config.internal.isRouter "Enable DHCP Server.";
+
+  };
+
+  config = mkIf cfg.enable {
+
+    #    internal.system.state.directories = [ "/var/lib/private/technitium-dns-server" ];
+    # services.technitium-dns-server = {
+    #   enable = true;
+    # };
+
+
+  internal.system.state.directories = [ "/var/lib/private/kea" ];
+  services.kea = {
+    dhcp4 = {
+      enable = true;
+      settings = {
+        lease-database = {
+          name = "/var/lib/kea/dhcp4.leases";
+          persist = true;
+          type = "memfile";
+        };
+        rebind-timer = 2000;
+        renew-timer = 1000;
+        valid-lifetime = 4000;
+
+        ddns-replace-client-name = "when-not-present";
+        ddns-override-client-update = true;
+        ddns-override-no-update = true;
+      };
+    };
+
+    dhcp-ddns = {
+      enable = true;
+      settings = {
+        ip-address = "127.0.0.11";
+        port = 53001;
+        dns-server-timeout = 100;
+        ncr-protocol = "UDP";
+        ncr-format = "JSON";
+
+        control-socket = {
+          socket-type = "unix";
+          socket-name = "/run/kea/ddns.socket";
+        };
+
+        forward-ddns = {
+          ddns-domains = [{
+            name = "home.net.r505.de.";
+            dns-servers = [{
+              hostname = "";
+              ip-address = "127.0.0.11";
+              port = 53;
+            }];
+          }];
+        };
+        reverse-ddns = {
+          ddns-domains = [{
+            name = "2.168.192.in-addr.arpa.";
+            dns-servers = [{
+              hostname = "";
+              ip-address = "127.0.0.11";
+              port = 53;
+            }];
+          }];
+        };
+      };
+    };
+  };
+
+    networking.interfaces.lo.ipv4.addresses = [{
+    address = "127.0.0.11";
+    prefixLength = 8;
+  }];
+
+  services.knot = {
+    enable = true;
+    settings = {
+      server.listen = [ "127.0.0.11@53" ];
+      acl = {
+        internal_ddns_transfer = {
+          address = [ "127.0.0.1" ];
+          action = [ "update" "transfer" ];
+        };
+      };
+      template = {
+        home = {
+          semantic-checks = true;
+          acl = [ "internal_ddns_transfer" ];
+          zonefile-load = "none";
+          zonefile-sync = -1;
+          journal-content = "all";
+        };
+      };
+      zone = {
+
+      };
+    };
+
+  };
+
+
+
+}
