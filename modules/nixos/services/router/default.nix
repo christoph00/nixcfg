0916a9ed831900ca42ef752d54ce66fe5666154c
christoph00 <christoph@asche.co>
2024-12-06 15:16:19 +0100

    ok

--- modules/nixos/services/router/default.nix
+++ modules/nixos/services/router/default.nix
@@ -1,27 +1,28 @@
 {
   # Snowfall Lib provides a customized `lib` instance with access to your flake's library
   # as well as the libraries available from your flake's inputs.
-  lib,
-  # An instance of `pkgs` with your overlays and packages applied is also available.
-  pkgs,
-  # You also have access to your flake's inputs.
-  inputs,
-  # Additional metadata is provided by Snowfall Lib.
-  namespace,
-  # The namespace used for your flake, defaulting to "internal" if not set.
-  system,
-  # The system architecture for this host (eg. `x86_64-linux`).
-  target,
-  # The Snowfall Lib target for this system (eg. `x86_64-iso`).
-  format,
-  # A normalized name for the system target (eg. `iso`).
-  virtual,
-  # A boolean to determine whether this system is a virtual target using nixos-generators.
-  systems, # An attribute map of your defined hosts.
+  lib
+, # An instance of `pkgs` with your overlays and packages applied is also available.
+  pkgs
+, # You also have access to your flake's inputs.
+  inputs
+, # Additional metadata is provided by Snowfall Lib.
+  namespace
+, # The namespace used for your flake, defaulting to "internal" if not set.
+  system
+, # The system architecture for this host (eg. `x86_64-linux`).
+  target
+, # The Snowfall Lib target for this system (eg. `x86_64-iso`).
+  format
+, # A normalized name for the system target (eg. `iso`).
+  virtual
+, # A boolean to determine whether this system is a virtual target using nixos-generators.
+  systems
+, # An attribute map of your defined hosts.
 
   # All other arguments come from the module system.
-  config,
-  ...
+  config
+, ...
 }:
 
 with builtins;
@@ -145,7 +146,7 @@ in
       networks."10-external" = {
         name = cfg.externalInterface;
         DHCP = "no";
-        addresses = [ { Address = "10.10.1.2/24"; } ];
+        addresses = [{ Address = "10.10.1.2/24"; }];
         vlan = [ "dtag-wan" ];
         linkConfig.MTUBytes = toString 1600;
       };
@@ -263,6 +264,8 @@ in
     systemd.services."pppd-dtag" = {
       partOf = [ "systemd-networkd.service" ];
     };
+
+    internal.services.allowedServices = [ "pppd-dtag" ];
 
     systemd.services.check-internet = {
       description = "check ipv4 internet connectivity";
