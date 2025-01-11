49f5991222c592b74222cd552fd4b5a3e5c5ba0b
christoph00 <christoph@asche.co>
2025-01-11 22:15:07 +0100

    ok

--- systems/x86_64-linux/oc2/default.nix
+++ systems/x86_64-linux/oc2/default.nix
@@ -7,8 +7,22 @@
   networking.hostName = "oc2";
 
   internal.type = "vm";
+  internal.system.fs.enable = false;
   internal.system.fs.device = "/dev/sda";
   internal.system.boot.encryptedRoot = false;
+  internal.system.fs.tmpRoot = false;
+  internal.system.fs.swapSize = "1G";
+  internal.fs.type = "xfs";
+
+  fileSystems."/boot" = {
+    device = "/dev/sda1";
+    fsType = "vfat";
+  };
+  fileSystems."/" = {
+    device = "/dev/sda3";
+    fsType = "xfs";
+  };
+  swapDevices = [ { device = "/dev/sda2"; } ];
 
   boot.initrd.kernelModules = [
     "ata_piix"
