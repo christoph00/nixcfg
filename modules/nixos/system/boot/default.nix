efdb3ad3668c3e669d13acd25c897dbda804d874
christoph00 <christoph@asche.co>
2025-01-12 15:17:40 +0100

    ok

--- modules/nixos/system/boot/default.nix
+++ modules/nixos/system/boot/default.nix
@@ -92,7 +92,7 @@ in
         patches = old.patches ++ [
           (pkgs.fetchurl {
             url = "https://github.com/wrvsrx/systemd/compare/tag_fix-hibernate-resume%5E...tag_fix-hibernate-resume.patch";
-            hash = "sha256-iDC5lenk4LhDdU4ZRHjestYu2jXNvNZCgSCkE2hQHuQ=";
+            hash = "sha256-Z784xysVUOYXCoTYJDRb3ppGiR8CgwY5CNV8jJSLOXU=";
           })
         ];
       });
