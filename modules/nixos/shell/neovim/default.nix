4cfd4b3ab09a9f52e786e8f9281ef7af59376481
christoph00 <christoph@asche.co>
2025-02-18 14:07:43 +0100

    ok

--- modules/nixos/shell/neovim/default.nix
+++ modules/nixos/shell/neovim/default.nix
@@ -80,6 +80,15 @@ in
             style = "day";
           };
 
+          autocomplete.blink-cmp = {
+            enable = true;
+            setupOpts = {
+              signature.enabled = true;
+              keymap.preset = "enter";
+
+            };
+          };
+
           languages = {
             # Options applied to all languages
             enableLSP = true;
@@ -108,7 +117,7 @@ in
             formatOnSave = true;
             # lspkind.enable = true;
             trouble.enable = true;
-            lspSignature.enable = true;
+            lspSignature.enable = false;
             otter-nvim.enable = true;
           };
 
@@ -120,71 +129,12 @@ in
             grammars = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
               yaml # Affects obsidian note frontmatter
               latex
+              nix
+              php
             ];
           };
-          lazy.plugins = {
-            "blink.cmp" = {
-              package = pkgs.vimPlugins.blink-cmp;
-              # event = [ "LspAttach" ];
-              # ft = [ "markdown" ];
-              event = [ "BufEnter" ];
-              setupModule = "blink.cmp";
-
-              setupOpts = {
-                keymap =
-                  let
-                    fallback = a: [
-                      a
-                      "fallback"
-                    ];
-                  in
-                  {
-                    preset = "none";
-                    "<C-j>" = fallback "select_next";
-                    "<C-k>" = fallback "select_prev";
-                    "<CS-j>" = fallback "scroll_documentation_down";
-                    "<CS-k>" = fallback "scroll_documentation_up";
-                    "<C-space>" = [
-                      "show"
-                      "show_documentation"
-                      "hide_documentation"
-                    ];
-                    "<C-e>" = [ "hide" ];
-                    "<C-y>" = [ "select_and_accept" ];
-                  };
-
-                # snippets.preset = "luasnip";
-
-                sources = {
-                  default = [
-                    "lsp"
-                    "path"
-                    # "snippets"
-                    "buffer"
-                  ];
-                  cmdline = [ ];
-
-                };
-
-                completion = {
-                  menu = {
-                    auto_show = true;
-                  };
-
-                  documentation = {
-                    auto_show = true;
-                    auto_show_delay_ms = 500;
-                  };
-
-                  ghost_text.enabled = false;
-                };
-              };
-            };
-          };
         };
-
       };
-
     };
   };
 }
