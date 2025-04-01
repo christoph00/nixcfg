6bc42ea41842b92b9dedbc2100e20ab443215aa2
christoph00 <christoph@asche.co>
2025-04-01 16:36:59 +0200

    ok

---
+++ modules/nixos/services/nas/default.nix
@@ -0,0 +1,91 @@
+{
+  # Snowfall Lib provides a customized `lib` instance with access to your flake's library
+  # as well as the libraries available from your flake's inputs.
+  lib,
+  # An instance of `pkgs` with your overlays and packages applied is also available.
+  pkgs,
+  # You also have access to your flake's inputs.
+  inputs,
+  # Additional metadata is provided by Snowfall Lib.
+  namespace,
+  # The namespace used for your flake, defaulting to "internal" if not set.
+  system,
+  # The system architecture for this host (eg. `x86_64-linux`).
+  target,
+  # The Snowfall Lib target for this system (eg. `x86_64-iso`).
+  format,
+  # A normalized name for the system target (eg. `iso`).
+  virtual,
+  # A boolean to determine whether this system is a virtual target using nixos-generators.
+  systems, # An attribute map of your defined hosts.
+
+  # All other arguments come from the module system.
+  config,
+  ...
+}:
+
+with builtins;
+with lib;
+with lib.internal;
+
+let
+  cfg = config.internal.services.nas;
+
+in
+{
+
+  options.internal.services.nas = {
+    enable = mkBoolOpt false "Enable NAS Services.";
+
+  };
+
+  config = mkIf cfg.enable {
+
+    internal.system.state.directories = [ "/var/lib/sftpgo" ];
+
+    age.secrets.sftpgo.file = ../../../../secrets/sftpgo.env;
+    systemd.services.sftpgo.serviceConfig.EnvironmentFile = config.age.secrets.sftpgo.path;
+
+    services.sftpgo = {
+      enable = true;
+      user = "sftpgo";
+      dataDir = "/var/lib/sftpgo";
+      settings = {
+        defender.enable = true;
+        data_provider = {
+          driver = "sqlite";
+          name = "sftpgo.db";
+          password_hashing = {
+            algo = "argon2id";
+            argon2_options = {
+              memory = 65536;
+              iterations = 2;
+              parallelism = 2;
+            };
+          };
+          password_caching = true;
+          create_default_admin = true;
+        };
+        webdavd.bindings = [
+          {
+            address = "127.0.0.1";
+            port = 5101;
+          }
+        ];
+        httpd.bindings = [
+          {
+            address = "127.0.0.1";
+            enable_https = false;
+            port = 5102;
+            client_ip_proxy_header = "X-Forwarded-For";
+            enable_web_admin = true;
+            enable_web_client = true;
+            enable_rest_api = true;
+          }
+        ];
+      };
+    };
+
+  };
+
+}
