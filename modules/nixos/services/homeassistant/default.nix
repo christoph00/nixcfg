4b94aca4180713c404e8d027c40ef03e32d89a0a
christoph00 <christoph@asche.co>
2025-04-25 15:54:42 +0200

    ok

--- modules/nixos/services/homeassistant/default.nix
+++ modules/nixos/services/homeassistant/default.nix
@@ -94,13 +94,6 @@ in
         proxyPass = "http://127.0.0.1:8123";
         recommendedProxySettings = true;
         proxyWebsockets = true;
-        extraConfig = ''
-          proxy_set_header        X-Real-IP $remote_addr;
-          proxy_set_header        X-Forwarded-For $proxy_add_x_forwarded_for;
-          proxy_set_header        X-Forwarded-Proto $scheme;
-          proxy_set_header        X-Forwarded-Host $host;
-          proxy_set_header        X-Forwarded-Server $host;
-        '';
       };
     };
 
