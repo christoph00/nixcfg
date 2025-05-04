1149739633620b1f7edb746d3137df120c3e646f
christoph00 <christoph@asche.co>
2025-05-04 19:39:17 +0200

    ok

--- systems/x86_64-linux/oc1/default.nix
+++ systems/x86_64-linux/oc1/default.nix
@@ -8,6 +8,7 @@
   internal.system.fs.device = "/dev/sda";
   internal.system.boot.encryptedRoot = false;
   internal.system.fs.swapSize = "1G";
+  internal.system.fs.tmpRoot = false;
 
   boot.initrd.kernelModules = [
     "ata_piix"
