88a234a13597258416c371b2505c444dc476eb91
christoph00 <christoph@asche.co>
2025-05-04 19:51:49 +0200

    ok

--- systems/x86_64-linux/oc2/default.nix
+++ systems/x86_64-linux/oc2/default.nix
@@ -4,10 +4,11 @@
 
   networking.hostName = "oc2";
 
-  internal.type = "vm";
+  internal.type = "bootstrap";
   internal.system.fs.device = "/dev/sda";
   internal.system.boot.encryptedRoot = false;
   internal.system.fs.swapSize = "1G";
+  internal.system.fs.tmpRoot = false;
 
   boot.initrd.kernelModules = [
     "ata_piix"
