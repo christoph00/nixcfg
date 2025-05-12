07b32252c87433a95cdb530935749dc4b5e430e4
christoph00 <christoph@asche.co>
2025-05-12 11:16:47 +0200

    ok

--- hosts/oc2/configuration.nix
+++ hosts/oc2/configuration.nix
@@ -7,4 +7,47 @@
 
   sys.state.enable = false;
   sys.disk.type = "xfs";
+
+  sys.disk.device = "/dev/sda";
+  sys.disk.forceDevice = true;
+
+  boot.kernelParams = [
+    "nvme.shutdown_timeout=10"
+    "nvme_core.shutdown_timeout=10"
+    "libiscsi.debug_libiscsi_eh=1"
+    "crash_kexec_post_notifiers"
+
+    # VNC console
+    "console=tty1"
+
+    # x86_64-linux
+    "console=ttyS0"
+
+    # aarch64-linux
+    # "console=ttyAMA0,115200"
+  ];
+
+  networking.timeServers = [ "169.254.169.254" ];
+
+  boot.initrd = {
+    availableKernelModules = [
+      "ata_piix"
+      "uhci_hcd"
+      "xen_blkfront"
+      "vmw_pvscsi"
+      "virtio_net"
+      "virtio_pci"
+      "virtio_blk"
+      "virtio_scsi"
+      "9p"
+      "9pnet_virtio"
+
+    ];
+    kernelModules = [
+      "nvme"
+      "virtio_balloon"
+      "virtio_console"
+      "virtio_rng"
+    ];
+  };
 }
