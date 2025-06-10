51154a062b4fafb79396e6ef37911f2a1765ca4c
christoph00 <christoph@asche.co>
2025-06-10 18:30:05 +0200

    ok

---
+++ modules/nixos/network/tailnet.nix
@@ -0,0 +1,38 @@
+{
+  config,
+  lib,
+  flake,
+  ...
+}:
+let
+  inherit (lib) mkIf boolToString mkForce;
+  inherit (flake.lib) mkBoolOpt mkStrOpt mkSecret;
+  cfg = config.network.tailnet;
+in
+{
+  options.network.tailnet = {
+    enable = mkBoolOpt true;
+    ip = mkStrOpt "100.64.64.0";
+  };
+
+  config = mkIf cfg.enable {
+    sys.state.directories = [ "/var/lib/tailscale" ];
+
+    networking.firewall = {
+      checkReversePath = "loose";
+      allowedUDPPorts = [ 41641 ];
+    };
+    services.tailscale = {
+      enable = true;
+      interfaceName = "ts0";
+      authKeyParameters.baseURL = "https://hs.r505.de";
+    };
+    age.secrets."tailscale-key" = {
+      owner = "tailscale";
+      file = "tailscale-key";
+    };
+
+    networking.firewall.trustedInterfaces = [ "ts0" ];
+  };
+
+}
