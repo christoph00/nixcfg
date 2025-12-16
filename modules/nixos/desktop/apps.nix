e6830b491986d0b9bb7dad22936f365e948c9fdd
christoph00 <christoph@asche.co>
2025-12-16 18:32:29 +0100

    ok

--- modules/nixos/desktop/apps.nix
+++ modules/nixos/desktop/apps.nix
@@ -44,10 +44,38 @@ in {
 
       vscode
 
-      ghostty
+      gohufont
     ];
 
-    # for foot themes
-    environment.pathsToLink = ["share/foot"];
+    # foot
+    environment.etc."xdg/foot/foot.ini".text = pkgs.lib.generators.toINI {} {
+      main = {
+        font = "BlexMono Nerd Font:size=11";
+        pad = "8x8";
+      };
+      colors = {
+        # modus operandi
+        background = "ffffff";
+        foreground = "000000";
+        regular0 = "000000";
+        regular1 = "a60000";
+        regular2 = "005e00";
+        regular3 = "813e00";
+        regular4 = "0031a9";
+        regular5 = "721045";
+        regular6 = "00538b";
+        regular7 = "bfbfbf";
+        bright0 = "595959";
+        bright1 = "972500";
+        bright2 = "315b00";
+        bright3 = "70480f";
+        bright4 = "2544bb";
+        bright5 = "5317ac";
+        bright6 = "005a5f";
+        bright7 = "ffffff";
+
+        jump-labels = "dce0e8 0000ff";
+      };
+    };
   };
 }
