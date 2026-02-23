46b7d1d2ed7265d6ea31c2eb5f44d5453d99a423
christoph00 <christoph@asche.co>
2026-02-23 14:18:37 +0100

    ok

--- modules/nixos/virt/podman.nix
+++ modules/nixos/virt/podman.nix
@@ -4,12 +4,14 @@
   inputs,
   pkgs,
   ...
-}: let
+}:
+let
   inherit (lib) mkIf;
 
   cfg = config.virt;
-in {
-  imports = [inputs.quadlet-nix.nixosModules.quadlet];
+in
+{
+  imports = [ inputs.quadlet-nix.nixosModules.quadlet ];
   config = mkIf cfg.podman {
     sys.state.directories = [
       "/var/lib/containers"
@@ -27,9 +29,22 @@ in {
 
     networking.firewall.trustedInterfaces = [ "podman0" ];
 
-
     virtualisation.podman = {
       enable = true;
+      defaultNetwork.settings = {
+        dns_enabled = true;
+        ipv6_enabled = true;
+        subnets = [
+          {
+            subnet = "10.88.0.0/16";
+            gateway = "10.88.0.1";
+          }
+          {
+            subnet = "fd00::/80";
+            gateway = "fd00::1";
+          }
+        ];
+      };
     };
   };
 }
