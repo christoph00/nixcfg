45e1de6bbbb2fd9760a218e73cb8bd33755a118c
christoph00 <christoph@asche.co>
2026-07-07 16:09:35 +0200

    remove nix caches

--- modules/nixos/system/nix.nix
+++ modules/nixos/system/nix.nix
@@ -87,17 +87,13 @@ in {
       substituters = [
         "https://chr.cachix.org"
         "https://nix-community.cachix.org"
-        "https://cache.garnix.io"
         "https://numtide.cachix.org"
-        "https://attic.xuyh0120.win/lantian"
       ];
 
       trusted-public-keys = [
-        "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
         "chr.cachix.org-1:8Z0vNVd8hK8lVU53Y26GHDNc6gv3eFzBNwSYOcUvsgA="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
-        "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
       ];
     };
   };
